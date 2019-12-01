bc952a8ffebb1f887c4baf6093ab010267b1b73e
Colm Baston <colm@colmbaston.co.uk>
2019-12-01 08:46:18 +0000

    puzzle 1

---
+++ puzzle01/src/main.rs
@@ -0,0 +1,19 @@
+fn main()
+{
+    let input : Vec<u64> = std::fs::read_to_string("input.txt").unwrap().lines().map(|x| x.parse().unwrap()).collect();
+
+    println!("{}", input.iter().map(|x| x / 3 - 2).sum::<u64>());
+    println!("{}", input.iter().map(|x|  fuel(*x)).sum::<u64>());
+}
+
+fn fuel(mut x : u64) -> u64
+{
+    let mut sum = 0;
+    while x > 6
+    {
+        x = x / 3 - 2;
+        sum += x;
+    }
+
+    sum
+}
