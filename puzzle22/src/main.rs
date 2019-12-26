823464ef5e20fb12a873cc4fe30b4a75f73b249f
Colm Baston <colm@colmbaston.uk>
2019-12-26 11:59:02 +0000

    puzzle 22 part 1 with linear function composition

--- puzzle22/src/main.rs
+++ puzzle22/src/main.rs
@@ -1,31 +1,33 @@
 use parsing::*;
 
-const DECK : i64 = 10_007;
+const DECK_ONE : i64 = 10_007;
+const _DECK_TWO : i64 = 119_315_717_514_047;
+const _SHUFFLES : i64 = 101_741_582_076_661;
 
 fn main()
 {
-    let input : Vec<Technique> = include_str!("../input.txt").lines().map(|s|
+    let input : Vec<Linear> = include_str!("../input.txt").lines().map(|s|
     {
-        alt((map(tag("deal into new stack"),                     |_| Technique::DealIntoNewStack),
-             map(preceded(tag("cut "),                 integer), |c| Technique::Cut(c)),
-             map(preceded(tag("deal with increment "), integer), |i| Technique::DealWithIncrement(i))))(s).unwrap().1
+        alt((map(tag("deal into new stack"),                     |_| Linear { a: -1, b: -1 }),
+             map(preceded(tag("cut "),                 integer), |c| Linear { a:  1, b: -c }),
+             map(preceded(tag("deal with increment "), integer), |i| Linear { a:  i, b:  0 })))(s).unwrap().1
     })
     .collect();
 
-    println!("{}", (input.iter().fold(2019, |a, t|
-    {
-        match *t
-        {
-            Technique::DealIntoNewStack     => (-a - 1) % DECK,
-            Technique::Cut(c)               => ( a - c) % DECK,
-            Technique::DealWithIncrement(i) => ( a * i) % DECK
-        }
-    }) + DECK) % DECK);
+    let shuffle = input.iter().fold(Linear { a: 1, b: 0 }, |x, y| x.compose_mod(y, DECK_ONE));
+    println!("{}", ((shuffle.a * 2019 + shuffle.b) % DECK_ONE + DECK_ONE) % DECK_ONE);
 }
 
-enum Technique
+struct Linear
 {
-    DealIntoNewStack,
-    Cut(i64),
-    DealWithIncrement(i64)
+    a : i64,
+    b : i64
+}
+
+impl Linear
+{
+    fn compose_mod(self, other : &Linear, n : i64) -> Linear
+    {
+        Linear { a: (self.a * other.a) % n, b: (other.a * self.b + other.b) % n }
+    }
 }
