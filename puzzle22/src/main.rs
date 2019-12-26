81fc6999b67dbe49d133a762ae51219c602778f0
Colm Baston <colm@colmbaston.uk>
2019-12-26 14:45:25 +0000

    puzzle 22 part 2

--- puzzle22/src/main.rs
+++ puzzle22/src/main.rs
@@ -1,33 +1,89 @@
 use parsing::*;
+use std::ops::RemAssign;
 
-const DECK_ONE : i64 = 10_007;
-const _DECK_TWO : i64 = 119_315_717_514_047;
-const _SHUFFLES : i64 = 101_741_582_076_661;
+const DECK_ONE : i128 = 10_007;
+const DECK_TWO : i128 = 119_315_717_514_047;
+const SHUFFLES : u64  = 101_741_582_076_661;
 
 fn main()
 {
     let input : Vec<Linear> = include_str!("../input.txt").lines().map(|s|
     {
-        alt((map(tag("deal into new stack"),                     |_| Linear { a: -1, b: -1 }),
-             map(preceded(tag("cut "),                 integer), |c| Linear { a:  1, b: -c }),
-             map(preceded(tag("deal with increment "), integer), |i| Linear { a:  i, b:  0 })))(s).unwrap().1
+        alt((map(tag("deal into new stack"),                     |_| Linear { a: -1,         b: -1         }),
+             map(preceded(tag("cut "),                 integer), |c| Linear { a:  1,         b: -c as i128 }),
+             map(preceded(tag("deal with increment "), integer), |i| Linear { a:  i as i128, b:  0         })))(s).unwrap().1
     })
     .collect();
 
-    let shuffle = input.iter().fold(Linear { a: 1, b: 0 }, |x, y| x.compose_mod(y, DECK_ONE));
-    println!("{}", ((shuffle.a * 2019 + shuffle.b) % DECK_ONE + DECK_ONE) % DECK_ONE);
+    let mut shuffle = input.iter().fold(Linear::IDENTITY, |mut a, x| { a.compose(x); a %= DECK_ONE; a });
+    shuffle.modulus(DECK_ONE);
+    println!("{}", (shuffle.a * 2019 + shuffle.b) % DECK_ONE);
+
+    let mut shuffle = input.iter().fold(Linear::IDENTITY, |mut a, x| { a.compose(x); a %= DECK_TWO; a });
+    shuffle.exp_by_squaring_mod(SHUFFLES, DECK_TWO);
+    let inverse = egcd(shuffle.a, DECK_TWO).1;
+    println!("{:?}", ((inverse * 2020 - inverse * shuffle.b) % DECK_TWO + DECK_TWO) % DECK_TWO);
 }
 
+#[derive(Clone)]
 struct Linear
 {
-    a : i64,
-    b : i64
+    a : i128,
+    b : i128
 }
 
 impl Linear
 {
-    fn compose_mod(self, other : &Linear, n : i64) -> Linear
+    const IDENTITY : Linear = Linear { a: 1, b: 0 };
+
+    fn compose(&mut self, other : &Linear)
     {
-        Linear { a: (self.a * other.a) % n, b: (other.a * self.b + other.b) % n }
+        *self = Linear { a: self.a * other.a, b: other.a * self.b + other.b }
+    }
+
+    fn exp_by_squaring_mod(&mut self, mut exponent : u64, n : i128)
+    {
+        let mut x = Linear::IDENTITY;
+        while exponent > 1
+        {
+            if exponent % 2 == 1
+            {
+                x.compose(&self);
+                x %= n;
+            }
+            self.compose(&self.clone());
+            *self %= n;
+            exponent /= 2;
+        }
+        self.compose(&x);
+        self.modulus(n)
+    }
+
+    fn modulus(&mut self, n : i128)
+    {
+        self.a = (self.a % n + n) % n;
+        self.b = (self.b % n + n) % n;
+    }
+}
+
+impl RemAssign<i128> for Linear
+{
+    fn rem_assign(&mut self, n : i128)
+    {
+        self.a %= n;
+        self.b %= n;
+    }
+}
+
+fn egcd(a : i128, b : i128) -> (i128, i128, i128)
+{
+    if a == 0
+    {
+        (b, 0, 1)
+    }
+    else
+    {
+        let (g, x, y) = egcd(b % a, a);
+        (g, y - (b / a) * x, x)
     }
 }
