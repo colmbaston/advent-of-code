231cf9422b967d19de54c8eb62ac631029fbae56
Colm Baston <colm@colmbaston.uk>
2019-12-29 09:40:53 +0000

    run script now diffs with correct answers

--- run.sh
+++ run.sh
@@ -6,16 +6,20 @@ COLOUR1="\e[1;31m"
 COLOUR2="\e[1;32m"
 
 cargo build --release
+rm -f output.txt
 
 if [ $? -eq 0 ];
 then
   time (for puzzle in $(ls | grep puzzle)
   do
     printf $COLOUR1
-    ./target/release/$puzzle
+    ./target/release/$puzzle | tee -a output.txt
     TEMP=$COLOUR1
     COLOUR1=$COLOUR2
     COLOUR2=$TEMP
   done
   printf "\e[0m")
 fi
+
+diff -w --color answers.txt output.txt
+rm output.txt
