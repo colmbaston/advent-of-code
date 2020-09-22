7eb9c7b31bf60f36b257d18640dc68929b08b7e7
Colm Baston <colm@colmbaston.uk>
2020-09-22 16:24:30 +0100

    inlined count() function and removed itertools dependency

--- 2018/puzzle02/src/main.rs
+++ 2018/puzzle02/src/main.rs
@@ -1,52 +1,49 @@
-use itertools::Itertools;
-
 fn main()
 {
     let input = include_str!("../input.txt");
 
     let (x, y) = input.lines().fold((0, 0), |(x, y), s|
     {
-        let (a, b) = count(s);
+        let mut histogram = [0 ; 26];
+        s.bytes().for_each(|c| histogram[(c - b'a') as usize] += 1);
+
+        let (a, b) = histogram.iter().try_fold((false, false), |(a, b), x|
+        {
+            match x
+            {
+                2 => if b { Err((true, true)) } else { Ok((true, b)) },
+                3 => if a { Err((true, true)) } else { Ok((a, true)) },
+                _ => Ok((a, b))
+            }
+        })
+        .unwrap_or_else(|x| x);
+
         (if a { x+1 } else { x }, if b { y+1 } else { y })
     });
     println!("{}", x * y);
 
-    'outer: for (s, t) in input.lines().tuple_combinations()
+    'outer: for (i, s) in input.lines().enumerate()
     {
-        let mut diff = None;
-        for (i, (c, d)) in s.bytes().zip(t.bytes()).enumerate()
+        'inner: for t in input.lines().skip(i+1)
         {
-            if c != d
+            let mut diff = None;
+            for (j, (c, d)) in s.bytes().zip(t.bytes()).enumerate()
             {
-                diff = match diff
+                if c != d
                 {
-                    None => Some(i),
-                    _    => continue 'outer
+                    diff = match diff
+                    {
+                        None => Some(j),
+                        _    => continue 'inner
+                    }
                 }
             }
-        }
 
-        if let Some(i) = diff
-        {
-            println!("{}{}", &s[..i], &s[i+1..]);
-            break
+            if let Some(j) = diff
+            {
+                println!("{}{}", &s[..j], &s[j+1..]);
+                break 'outer
+            }
         }
     }
-}
-
-fn count(s : &str) -> (bool, bool)
-{
-    let mut histogram = [0 ; 26];
-    s.bytes().for_each(|c| histogram[(c - b'a') as usize] += 1);
-
-    histogram.iter().try_fold((false, false), |(a, b), x|
-    {
-        match x
-        {
-            2 => if b { Err((true, true)) } else { Ok((true, b)) },
-            3 => if a { Err((true, true)) } else { Ok((a, true)) },
-            _ => Ok((a, b))
-        }
-    })
-    .unwrap_or_else(|x| x)
 }
