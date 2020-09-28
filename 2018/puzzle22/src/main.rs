ad0c0f51ceeee258c15d07630916feefde23bd52
Colm Baston <colm@colmbaston.uk>
2020-09-28 23:28:02 +0100

    now parsing input from file with nom

--- 2018/puzzle22/src/main.rs
+++ 2018/puzzle22/src/main.rs
@@ -1,56 +1,73 @@
 use std::collections::{ HashSet, HashMap, BinaryHeap };
 
-const DEPTH  :  u32       =  10914;
-const TARGET : (i32, i32) = (9, 739);
-
 fn main()
 {
-    // a cache of the cave's geologic indices
+    // parse input from statically-included file
+    let (_, cave) = parse_cave(include_str!("../input.txt")).unwrap();
+    let (tx, ty)  = cave.target;
+
+    // a cache of the cave's erosion levels
     let mut cache = HashMap::new();
 
-    // force the caching of the rectangle from (0, 0) to TARGET
-    geologic_index(&mut cache, TARGET);
-    geologic_index(&mut cache, (TARGET.0, TARGET.1 - 1));
-    geologic_index(&mut cache, (TARGET.0 - 1, TARGET.1));
+    // force the caching of the rectangle from (0, 0) to the target
+    // (0, 0) and the target won't be present in the cache, but they don't change the result
+    erosion_level(&mut cache, (tx, ty - 1), &cave);
+    erosion_level(&mut cache, (tx - 1, ty), &cave);
 
-    // part one: sum the risk levels of the rectangle from (0, 0) to TARGET
+    // part one: sum the risk levels of the rectangle from (0, 0) to the target
     println!("{}", cache.values().fold(0, |a, x| a + region_type(*x) as u32));
 
-    // part two: use A* search to find the number of minutes to reach TARGET
-    println!("{}", astar(&mut cache));
+    // part two: use A* search to find the number of minutes to reach target
+    println!("{}", astar(&mut cache, &cave));
+}
+
+struct Cave
+{
+    depth  :  u32,
+    target : (i32, i32)
+}
+
+fn parse_cave(s : &str) -> nom::IResult<&str, Cave>
+{
+    use nom::character::complete::digit1;
+
+    let (s, depth) = digit1(&s[7..])?;
+    let (s, tx)    = digit1(&s[9..])?;
+    let (s, ty)    = digit1(&s[1..])?;
+
+    Ok((s, Cave
+    {
+        depth:  depth.parse().unwrap(),
+        target: (tx.parse().unwrap(), ty.parse().unwrap())
+    }))
 }
 
 type Cache = HashMap<(i32, i32), u32>;
 
-fn geologic_index(cache : &mut Cache, position : (i32, i32)) -> u32
+fn erosion_level(cache : &mut Cache, position : (i32, i32), cave : &Cave) -> u32
 {
     match cache.get(&position)
     {
-        // if the geologic index is already present in the cache, return it
-        Some(index) => *index,
-        None        =>
+        // if the erosion level is already present in the cache, return it
+        Some(&erosion) => erosion,
+        None           =>
         {
             // otherwise compute it
-            let index = match position
+            let geologic = match position
             {
-                TARGET => 0,
-                (x, 0) => x as u32 * 16807,
-                (0, y) => y as u32 * 48271,
-                (x, y) => erosion_level(geologic_index(cache, (x-1, y)))
-                        * erosion_level(geologic_index(cache, (x, y-1)))
+                t if t == cave.target => 0,
+                (x, 0)                => x as u32 * 16807,
+                (0, y)                => y as u32 * 48271,
+                (x, y)                => erosion_level(cache, (x-1, y), cave)
+                                       * erosion_level(cache, (x, y-1), cave)
             };
+            let erosion = (geologic + cave.depth) % 20183;
 
             // and insert it into the cache before returning it
-            cache.insert(position, index);
-            index
+            cache.insert(position, erosion);
+            erosion
         }
     }
-}
-
-#[inline]
-fn erosion_level(geologic_index : u32) -> u32
-{
-    (geologic_index + DEPTH) % 20183
 }
 
 enum Region
@@ -61,17 +78,18 @@ enum Region
 }
 
 #[inline]
-fn region_type(geologic_index : u32) -> Region
+fn region_type(erosion : u32) -> Region
 {
-    match erosion_level(geologic_index) % 3
+    match erosion % 3
     {
         0 => Region::Rocky,
         1 => Region::Wet,
-        _ => Region::Narrow
+        2 => Region::Narrow,
+        _ => unreachable!()
     }
 }
 
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct State
 {
     position: (i32, i32),
@@ -89,7 +107,7 @@ enum Tool
 impl State
 {
     // generate possible moves from one state to the next, along with how many minutes each move takes
-    fn moves(&self, cache : &mut Cache) -> Vec<(State, u32)>
+    fn moves(&self, cache : &mut Cache, cave : &Cave) -> Vec<(State, u32)>
     {
         let (x, y) = self.position;
 
@@ -105,7 +123,7 @@ impl State
             else
             {
                 // you can only move into the adjacent region with an appropriate tool equipped
-                match (region_type(geologic_index(cache, position)), &self.tool)
+                match (region_type(erosion_level(cache, position, cave)), &self.tool)
                 {
                     (Region::Rocky,  Tool::Neither)      => None,
                     (Region::Wet,    Tool::Torch)        => None,
@@ -117,7 +135,7 @@ impl State
         .collect::<Vec<_>>();
 
         // you can switch to the other tool available for the current region in seven minutes
-        let tool = match (region_type(geologic_index(cache, self.position)), &self.tool)
+        let tool = match (region_type(erosion_level(cache, self.position, cave)), &self.tool)
         {
             (Region::Rocky,  Tool::Torch)        => Tool::ClimbingGear,
             (Region::Rocky,  Tool::ClimbingGear) => Tool::Torch,
@@ -134,13 +152,13 @@ impl State
 }
 
 #[inline]
-// use Manhattan distance to TARGET as the A* heuristic
-fn manhattan(&(x, y) : &(i32, i32)) -> u32
+// use Manhattan distance to the target as the A* heuristic
+fn manhattan((x1, y1) : (i32, i32), (x2, y2) : (i32, i32)) -> u32
 {
-    ((x - TARGET.0).abs() + (y - TARGET.1).abs()) as u32
+    ((x1 - x2).abs() + (y1 - y2).abs()) as u32
 }
 
-fn astar(cache : &mut Cache) -> u32
+fn astar(cache : &mut Cache, cave : &Cave) -> u32
 {
     use std::cmp::Reverse;
 
@@ -153,9 +171,9 @@ fn astar(cache : &mut Cache) -> u32
     while let Some((_, steps, state)) = queue.pop()
     {
         if visited.contains(&state) { continue }
-        if let State { position: TARGET, tool: Tool::Torch } = state { return steps }
+        if state.position == cave.target && state.tool == Tool::Torch { return steps }
 
-        queue.extend(state.moves(cache).into_iter().filter_map(|(state, k)|
+        queue.extend(state.moves(cache, cave).into_iter().filter_map(|(state, k)|
         {
             if visited.contains(&state)
             {
@@ -164,12 +182,12 @@ fn astar(cache : &mut Cache) -> u32
             else
             {
                 let steps = steps + k;
-                Some((Reverse(steps + manhattan(&state.position)), steps, state))
+                Some((Reverse(steps + manhattan(state.position, cave.target)), steps, state))
             }
         }));
 
         visited.insert(state);
     }
 
-    panic!("exhausted A* search without finding TARGET");
+    panic!("exhausted A* search without finding the target");
 }
