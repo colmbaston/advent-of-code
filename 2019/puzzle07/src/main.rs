03e578bb3c27db2e5e71dddca6d7471cecf51ff0
Colm Baston <colm@colmbaston.uk>
2020-10-15 23:14:23 +0100

    permutations module

--- 2019/puzzle07/src/main.rs
+++ 2019/puzzle07/src/main.rs
@@ -1,6 +1,9 @@
 use intcode::Interpreter;
 use std::sync::mpsc::channel;
 
+mod permutations;
+use permutations::Permutations;
+
 fn main()
 {
     let input = intcode::parse_file!("../input.txt");
@@ -42,46 +45,4 @@ fn run(input : &[i64], feedback : bool)
         }
     }
     println!("{}", max);
-}
-
-struct Permutations<T>
-{
-    data:   Option<Vec<T>>,
-    output: Option<Vec<T>>
-}
-
-impl<T> Permutations<T>
-{
-    fn new(values : impl Iterator<Item = T>) -> Permutations<T>
-    {
-        let data = Some(values.collect());
-        Permutations { data, output: None }
-    }
-}
-
-impl<T : Clone + Ord> Iterator for Permutations<T>
-{
-    type Item = Vec<T>;
-
-    fn next(&mut self) -> Option<Vec<T>>
-    {
-        if let Some(data) = &mut self.data
-        {
-            self.output = Some(data.clone());
-
-            match data.windows(2).enumerate().rev().find(|(_, w)| w[0] < w[1])
-            {
-                None         => self.data = None,
-                Some((k, _)) =>
-                {
-                    let data_k = &data[k];
-                    let (l, _) = data.iter().enumerate().rev().find(|(_, x)| data_k < x).unwrap();
-                    data.swap(k, l);
-                    data[k+1..].reverse();
-                }
-            }
-        }
-
-        self.output.take()
-    }
 }
