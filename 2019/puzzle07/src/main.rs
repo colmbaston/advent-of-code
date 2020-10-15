9aad7020dba38efea94ce30d4338bb76d4deaacb
Colm Baston <colm@colmbaston.uk>
2020-10-15 23:09:18 +0100

    wrote lexicographic permutation iterator

--- 2019/puzzle07/src/main.rs
+++ 2019/puzzle07/src/main.rs
@@ -1,5 +1,4 @@
 use intcode::Interpreter;
-use itertools::Itertools;
 use std::sync::mpsc::channel;
 
 fn main()
@@ -12,14 +11,14 @@ fn main()
 fn run(input : &[i64], feedback : bool)
 {
     let mut max = std::i64::MIN;
-    for ps in if feedback { 5 .. 10 } else { 0 .. 5 }.permutations(5)
+    for perm in Permutations::new(if feedback { 5 .. 10 } else { 0 .. 5 })
     {
         let (first_send, first_recv) = channel();
         let mut last_recv = first_recv;
         let mut last_send = first_send.clone();
         let mut handles   = Vec::new();
 
-        for x in ps
+        for x in perm.into_iter()
         {
             last_send.send(x).unwrap();
             let (sender, receiver) = channel();
@@ -43,4 +42,46 @@ fn run(input : &[i64], feedback : bool)
         }
     }
     println!("{}", max);
+}
+
+struct Permutations<T>
+{
+    data:   Option<Vec<T>>,
+    output: Option<Vec<T>>
+}
+
+impl<T> Permutations<T>
+{
+    fn new(values : impl Iterator<Item = T>) -> Permutations<T>
+    {
+        let data = Some(values.collect());
+        Permutations { data, output: None }
+    }
+}
+
+impl<T : Clone + Ord> Iterator for Permutations<T>
+{
+    type Item = Vec<T>;
+
+    fn next(&mut self) -> Option<Vec<T>>
+    {
+        if let Some(data) = &mut self.data
+        {
+            self.output = Some(data.clone());
+
+            match data.windows(2).enumerate().rev().find(|(_, w)| w[0] < w[1])
+            {
+                None         => self.data = None,
+                Some((k, _)) =>
+                {
+                    let data_k = &data[k];
+                    let (l, _) = data.iter().enumerate().rev().find(|(_, x)| data_k < x).unwrap();
+                    data.swap(k, l);
+                    data[k+1..].reverse();
+                }
+            }
+        }
+
+        self.output.take()
+    }
 }
