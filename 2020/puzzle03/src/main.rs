0cdfccef1b7a5bcf35beef1f4ea55ef6e676d7a9
Colm Baston <colm@colmbaston.uk>
2020-12-03 12:44:54 +0000

    minor style changes

--- 2020/puzzle03/src/main.rs
+++ 2020/puzzle03/src/main.rs
@@ -1,12 +1,12 @@
 fn main()
 {
-    let input  = include_str!("../input.txt").lines().map(|l| l.chars().map(|c| c == '#').collect::<Vec<_>>()).collect::<Vec<_>>();
+    let input = include_str!("../input.txt").lines().map(|l| l.bytes().map(|c| c == b'#').collect::<Vec<_>>()).collect::<Vec<_>>();
 
-    println!("{}", trees(&input, 3, 1));
-    println!("{}", [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)].iter().map(|&(n, d)| trees(&input, n, d)).product::<usize>());
+    println!("{}", trees(&input, (3, 1)));
+    println!("{}", [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)].iter().map(|&v| trees(&input, v)).product::<usize>());
 }
 
-fn trees(grid : &[Vec<bool>], right : usize, down : usize) -> usize
+fn trees(grid : &[Vec<bool>], (right, down) : (usize, usize)) -> usize
 {
     let width  = grid[0].len();
     let height = grid.len();
