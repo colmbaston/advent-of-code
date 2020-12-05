0640577df8d95f44ed558cfe142f96de0067b3c4
Colm Baston <colm@colmbaston.uk>
2020-12-05 10:30:35 +0000

    linear search using windows

--- 2020/puzzle05/src/main.rs
+++ 2020/puzzle05/src/main.rs
@@ -1,20 +1,10 @@
-fn main()
-{
-    let mut input = include_str!("../input.txt").lines()
-                                                .map(|s| s.bytes().fold(0, |a, x| 2 * a + (x == b'B' || x == b'R') as u32))
-                                                .collect::<Vec<_>>();
-
-    input.sort();
-    println!("{}", input[input.len() - 1]);
-
-    let mut prev = input[0];
-    for s in input.into_iter().skip(1)
+    fn main()
     {
-        if s - prev == 2
-        {
-            println!("{}", prev + 1);
-            break
-        }
-        prev = s;
+        let mut input = include_str!("../input.txt").lines()
+                                                    .map(|s| s.bytes().fold(0, |a, x| 2 * a + (x == b'B' || x == b'R') as u32))
+                                                    .collect::<Vec<_>>();
+
+        input.sort();
+        println!("{}", input[input.len() - 1]);
+        println!("{}", input.windows(2).find(|w| w[0] + 2 == w[1]).unwrap()[0] + 1);
     }
-}
