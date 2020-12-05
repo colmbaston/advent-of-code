4b7d43095f6897063f3ecd899dcaab4085c395a5
Colm Baston <colm@colmbaston.uk>
2020-12-05 16:58:13 +0000

    parsing by collecting iterators, using &str instead of String

--- 2020/puzzle04/src/main.rs
+++ 2020/puzzle04/src/main.rs
@@ -2,38 +2,33 @@ fn main()
 {
     let input = parse_passports(include_str!("../input.txt"));
 
-    let all_present = input.iter().filter(|p| p.iter().filter(|(k, _)| k != "cid").count() == 7);
+    let all_present = input.iter().filter(|p| p.iter().filter(|(k, _)| *k != "cid").count() == 7);
 
     println!("{}", all_present.clone().count());
     println!("{}", all_present.filter(|p| p.iter().all(valid_field)).count());
 }
 
-type Passport = Vec<(String, String)>;
+type Passport<'a> = Vec<(&'a str, &'a str)>;
 
 fn parse_passports(s : &str) -> Vec<Passport>
 {
-    let mut ps = Vec::new();
-
-    for p in s.split("\n\n")
+    s.split("\n\n").map(|p|
     {
-        let mut fs = Vec::new();
-        for f in p.split_whitespace()
+        p.split_whitespace().map(|f|
         {
             let mut x = f.split(':');
-            fs.push((x.next().unwrap().to_string(),
-                     x.next().unwrap().to_string()));
-        }
-        ps.push(fs);
-    }
-
-    ps
+            (x.next().unwrap(), x.next().unwrap())
+        })
+        .collect::<Vec<_>>()
+    })
+    .collect::<Vec<_>>()
 }
 
-fn valid_field((k, v) : &(String, String)) -> bool
+fn valid_field((k, v) : &(&str, &str)) -> bool
 {
     let n_digits = |n| if v.len() == n { v.parse::<u32>().ok() } else { None };
 
-    match k.as_str()
+    match *k
     {
         "byr" => n_digits(4).map(|k| 1920 <= k && k <= 2002),
         "iyr" => n_digits(4).map(|k| 2010 <= k && k <= 2020),
@@ -44,7 +39,7 @@ fn valid_field((k, v) : &(String, String)) -> bool
             None     => v.strip_suffix("in").and_then(|i| i.parse::<u8>().ok().map(|k|  59 <= k && k <=  76))
         },
         "hcl" => v.strip_prefix('#').map(|hcl| hcl.bytes().all(|c| c.is_ascii_hexdigit() && !c.is_ascii_uppercase())),
-        "ecl" => Some(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&v.as_str())),
+        "ecl" => Some(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&v)),
         "pid" => n_digits(9).map(|_| true),
         _     => Some(true),
     }
