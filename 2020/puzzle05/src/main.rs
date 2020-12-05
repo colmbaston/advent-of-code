2389169b44786b8b49f560d643c4898dc049cb79
Colm Baston <colm@colmbaston.uk>
2020-12-05 10:31:49 +0000

    fixed indentation

--- 2020/puzzle05/src/main.rs
+++ 2020/puzzle05/src/main.rs
@@ -1,10 +1,10 @@
-    fn main()
-    {
-        let mut input = include_str!("../input.txt").lines()
-                                                    .map(|s| s.bytes().fold(0, |a, x| 2 * a + (x == b'B' || x == b'R') as u32))
-                                                    .collect::<Vec<_>>();
+fn main()
+{
+    let mut input = include_str!("../input.txt").lines()
+                                                .map(|s| s.bytes().fold(0, |a, x| 2 * a + (x == b'B' || x == b'R') as u32))
+                                                .collect::<Vec<_>>();
 
-        input.sort();
-        println!("{}", input[input.len() - 1]);
-        println!("{}", input.windows(2).find(|w| w[0] + 2 == w[1]).unwrap()[0] + 1);
-    }
+    input.sort();
+    println!("{}", input[input.len() - 1]);
+    println!("{}", input.windows(2).find(|w| w[0] + 2 == w[1]).unwrap()[0] + 1);
+}
