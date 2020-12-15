f265c7dc386f2360e2cbdd86567e0d004b9f30a5
Colm Baston <colm@colmbaston.uk>
2020-12-15 16:39:00 +0000

    2020 day 15

---
+++ 2020/puzzle15/src/main.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+
+fn main()
+{
+    let input = include_str!("../input.txt").trim_end().split(',').map(|x| x.parse::<u32>().unwrap()).collect::<Vec<_>>();
+
+    let mut prev    = input.last().cloned().unwrap();
+    let mut visited = input[.. input.len() - 1].iter().cloned().zip(1..).collect::<HashMap<u32, u32>>();
+
+    for i in input.len() as u32 .. 2020 { prev = step(prev, i, &mut visited) }
+    println!("{}", prev);
+    for i in 2020 .. 30000000           { prev = step(prev, i, &mut visited) }
+    println!("{}", prev);
+}
+
+fn step(prev : u32, i : u32, visited : &mut HashMap<u32, u32>) -> u32
+{
+    match visited.insert(prev, i)
+    {
+        None    => 0,
+        Some(j) => i - j
+    }
+}
