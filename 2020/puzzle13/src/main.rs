423f4d880de7f38d46e5ec78d9c00f31a345c3eb
Colm Baston <colm@colmbaston.uk>
2020-12-17 12:05:17 +0000

    changed an iter() to into_iter()

--- 2020/puzzle13/src/main.rs
+++ 2020/puzzle13/src/main.rs
@@ -5,12 +5,12 @@ fn main()
     let (b, m) = bs.iter().filter_map(|b| b.map(|t| (t, t - k % t))).min_by(|x, y| x.1.cmp(&y.1)).unwrap();
     println!("{}", b * m);
 
-    let mut congs = bs.iter().zip(0..).filter_map(|(b, t)| b.map(|n| (n, (n - t % n) % n))).collect::<Vec<_>>();
+    let mut congs = bs.into_iter().zip(0..).filter_map(|(b, t)| b.map(|n| (n, (n - t % n) % n))).collect::<Vec<_>>();
     congs.sort_unstable();
     let (mut acc_n, mut acc_t) = congs.pop().unwrap();
     for (n, t) in congs.into_iter().rev()
     {
-        acc_t  = (0 ..).map(|k| k * acc_n + acc_t).find(|k| k % n == t).unwrap();
+        acc_t  = (0..).map(|k| k * acc_n + acc_t).find(|k| k % n == t).unwrap();
         acc_n *= n;
     }
     println!("{}", acc_t);
