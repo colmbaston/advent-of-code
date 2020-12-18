4976e4d85c0628d383ab067cf2efe9f86b8e8e2a
Colm Baston <colm@colmbaston.uk>
2020-12-18 20:17:44 +0000

    a general solution for both parts by prededence climbing

--- 2020/puzzle18/src/main.rs
+++ 2020/puzzle18/src/main.rs
@@ -1,9 +1,15 @@
+use std::collections::HashMap;
+
 fn main()
 {
     let input = include_str!("../input.txt").lines().collect::<Vec<_>>();
 
-    println!("{}", input.iter().map(|l| Expr::parse_one(l).unwrap().eval()).sum::<u64>());
-    println!("{}", input.iter().map(|l| Expr::parse_two(l).unwrap().eval()).sum::<u64>());
+    let mut ops = HashMap::new();
+    ops.insert('+', (Op::Add, 0));
+    ops.insert('*', (Op::Mul, 0));
+    println!("{}", input.iter().filter_map(|l| Expr::parse(l, &ops).map(|e| e.eval())).sum::<u64>());
+    ops.insert('+', (Op::Add, 1));
+    println!("{}", input.iter().filter_map(|l| Expr::parse(l, &ops).map(|e| e.eval())).sum::<u64>());
 }
 
 enum Expr
@@ -29,95 +35,6 @@ impl Expr
             Expr::Op(op, x, y) => op.apply(x.eval(), y.eval())
         }
     }
-
-    fn parse_one(s : &str) -> Option<Expr>
-    {
-        let (x, s) = Expr::parse_op_one(s.trim())?;
-        if s.is_empty() { Some(x) } else { None }
-    }
-
-    fn parse_op_one(s : &str) -> Option<(Expr, &str)>
-    {
-        let (mut x, mut s) = Expr::parse_atom_one(s)?;
-        let ops = [('+', Op::Add), ('*', Op::Mul)];
-
-        loop
-        {
-            s = s.trim_start();
-            match ops.iter().find_map(|(p, op)| s.strip_prefix(|c| c == *p).map(|s| (op, s)))
-            {
-                None          => break Some((x, s)),
-                Some((op, t)) =>
-                {
-                    let     t  = t.trim_start();
-                    let (y, t) = Expr::parse_atom_one(t)?;
-
-                    x = Expr::Op(op.clone(), Box::new(x), Box::new(y));
-                    s = t;
-                }
-            }
-        }
-    }
-
-    fn parse_atom_one(s : &str) -> Option<(Expr, &str)>
-    {
-        match s.strip_prefix('(')
-        {
-            None =>
-            {
-                let (ds, s) = s.split_at(s.find(|c : char| !c.is_ascii_digit()).unwrap_or_else(|| s.len()));
-                ds.parse().ok().map(|k| (Expr::Lit(k), s))
-            }
-            Some(s) =>
-            {
-                let (x, s) = Expr::parse_op_one(s.trim_start())?;
-                s.trim_start().strip_prefix(')').map(|s| (x, s))
-            }
-        }
-    }
-
-    fn parse_two(s : &str) -> Option<Expr>
-    {
-        let (x, s) = Expr::parse_mul_two(s.trim())?;
-        if s.is_empty() { Some(x) } else { None }
-    }
-
-    fn parse_mul_two(s : &str) -> Option<(Expr, &str)>
-    {
-        let (x, s) = Expr::parse_add_two(s)?;
-        match s.trim_start().strip_prefix('*')
-        {
-            None    => Some((x, s)),
-            Some(s) => Expr::parse_mul_two(s.trim_start()).map(|(y, s)| (Expr::Op(Op::Mul, Box::new(x), Box::new(y)), s))
-        }
-    }
-
-    fn parse_add_two(s : &str) -> Option<(Expr, &str)>
-    {
-        let (x, s) = Expr::parse_atom_two(s)?;
-        match s.trim_start().strip_prefix('+')
-        {
-            None    => Some((x, s)),
-            Some(s) => Expr::parse_add_two(s.trim_start()).map(|(y, s)| (Expr::Op(Op::Add, Box::new(x), Box::new(y)), s))
-        }
-    }
-
-    fn parse_atom_two(s : &str) -> Option<(Expr, &str)>
-    {
-        match s.strip_prefix('(')
-        {
-            None =>
-            {
-                let (ds, s) = s.split_at(s.find(|c : char| !c.is_ascii_digit()).unwrap_or_else(|| s.len()));
-                ds.parse().ok().map(|k| (Expr::Lit(k), s))
-            }
-            Some(s) =>
-            {
-                let (x, s) = Expr::parse_mul_two(s.trim_start())?;
-                s.trim_start().strip_prefix(')').map(|s| (x, s))
-            }
-        }
-    }
 }
 
 impl Op
@@ -128,6 +45,60 @@ impl Op
         {
             Op::Add => x + y,
             Op::Mul => x * y
+        }
+    }
+}
+
+type OpTable = HashMap<char, (Op, u8)>;
+
+impl Expr
+{
+    fn parse(s : &str, ops : &OpTable) -> Option<Expr>
+    {
+        let (x, s) = Expr::parse_ops(s.trim_start(), ops, 0)?;
+        if s.trim_start().is_empty() { Some(x) } else { None }
+    }
+
+    fn parse_ops<'a>(s : &'a str, ops : &OpTable, prec : u8) -> Option<(Expr, &'a str)>
+    {
+        let (mut x, mut s) = Expr::parse_atom(s, ops)?;
+
+        loop
+        {
+            s = s.trim_start();
+            match s.chars().next().and_then(|c| ops.get(&c))
+            {
+                None          => break,
+                Some((op, p)) => if *p < prec
+                {
+                    break
+                }
+                else
+                {
+                    let (y, t) = Expr::parse_ops(&s[1..].trim_start(), ops, *p + 1)?;
+                    x = Expr::Op(op.clone(), Box::new(x), Box::new(y));
+                    s = t;
+                }
+            }
+        }
+
+        Some((x, s))
+    }
+
+    fn parse_atom<'a>(s : &'a str, ops : &OpTable) -> Option<(Expr, &'a str)>
+    {
+        match s.strip_prefix('(')
+        {
+            None =>
+            {
+                let (ds, s) = s.split_at(s.find(|c : char| !c.is_ascii_digit()).unwrap_or_else(|| s.len()));
+                ds.parse().ok().map(|k| (Expr::Lit(k), s))
+            }
+            Some(s) =>
+            {
+                let (x, s) = Expr::parse_ops(s.trim_start(), ops, 0)?;
+                s.trim_start().strip_prefix(')').map(|s| (x, s))
+            }
         }
     }
 }
