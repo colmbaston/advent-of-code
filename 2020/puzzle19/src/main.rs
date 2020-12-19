06b1354b60ad4ab8dea2806fc493d0d325847dd5
Colm Baston <colm@colmbaston.uk>
2020-12-19 13:20:06 +0000

    2020 day 19

---
+++ 2020/puzzle19/src/main.rs
@@ -0,0 +1,70 @@
+use std::collections::HashMap;
+
+fn main()
+{
+    let (mut rules, strings) = parse_input(include_str!("../input.txt"));
+
+    println!("{}", strings.iter().filter(|src| rules.get(&0).unwrap().full_match(src, &rules)).count());
+    rules.insert(8,  Rule::SubRules(vec![vec![42],     vec![42, 8]]));
+    rules.insert(11, Rule::SubRules(vec![vec![42, 31], vec![42, 11, 31]]));
+    println!("{}", strings.iter().filter(|src| rules.get(&0).unwrap().full_match(src, &rules)).count());
+}
+
+fn parse_input(s : &str) -> (HashMap<u32, Rule>, Vec<&str>)
+{
+    let mut i = s.split("\n\n");
+
+    (i.next().unwrap().lines().map(Rule::parse).collect(),
+     i.next().unwrap().lines().collect())
+}
+
+enum Rule
+{
+    Char(char),
+    SubRules(Vec<Vec<u32>>)
+}
+
+impl Rule
+{
+    fn parse(s : &str) -> (u32, Rule)
+    {
+        let mut i = s.split(": ");
+        let id = i.next().unwrap().parse().unwrap();
+        let s  = i.next().unwrap();
+
+        (id, match s.strip_prefix('"')
+        {
+            None    => Rule::SubRules(s.split('|').map(|r| r.split_whitespace().map(|x| x.parse().unwrap()).collect()).collect()),
+            Some(s) => Rule::Char(s.chars().next().unwrap())
+        })
+    }
+
+    fn full_match(&self, src : &str, rules : &HashMap<u32, Rule>) -> bool
+    {
+        self.prefix_match(src, rules).into_iter().any(|s| s.is_empty())
+    }
+
+    fn prefix_match<'a>(&self, src : &'a str, rules : &HashMap<u32, Rule>) -> Vec<&'a str>
+    {
+        let mut matches = Vec::new();
+
+        match self
+        {
+            Rule::Char(c)      => matches.extend(src.strip_prefix(|d| *c == d).into_iter()),
+            Rule::SubRules(rs) => for alt in rs.iter()
+            {
+                let mut t = vec![src];
+
+                for id in alt.iter()
+                {
+                    let r = rules.get(&id).unwrap();
+                    t = t.into_iter().flat_map(|s| r.prefix_match(s, rules).into_iter()).collect();
+                }
+
+                matches.extend(t.into_iter())
+            }
+        }
+
+        matches
+    }
+}
