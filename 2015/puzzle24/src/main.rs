d88ad6863a5f34bb0dc1a3bca8bbc0ce43849246
Colm Baston <colm@colmbaston.uk>
2021-11-24 16:21:01 +0000

    track partial sums and products when finding combinations

--- 2015/puzzle24/src/main.rs
+++ 2015/puzzle24/src/main.rs
@@ -3,20 +3,18 @@ fn main()
     let input = include_str!("../input.txt").lines().map(|x| x.parse::<u64>().unwrap()).collect::<Vec<u64>>();
     for groups in 3 ..= 4
     {
-        let target     = input.iter().sum::<u64>() / groups;
-        let target_len = input.iter()
-                              .rev()
-                              .try_fold((0, 0), |(l, s), x| if s < target { Ok((l+1, s+x)) } else { Err((l, s)) })
-                              .unwrap_err().0;
+        let target = input.iter().sum::<u64>() / groups;
+        let initial_size = input.iter()
+                                .rev()
+                                .try_fold((0, 0), |(l, s), x| if s < target { Ok((l+1, s+x)) } else { Err((l, s)) })
+                                .unwrap_err().0;
 
-        for len in target_len ..
+        for size in initial_size ..
         {
             let mut cs = Vec::new();
-            combinations(&input, len, Vec::new(), &mut cs);
+            combinations(&input, size, target, 0, 1, &mut cs);
 
-            if let Some(qe) = cs.into_iter()
-                                .filter_map(|v| if v.iter().sum::<u64>() == target { Some(v.iter().product::<u64>()) } else { None })
-                                .min()
+            if let Some(qe) = cs.into_iter().filter_map(|(s, p)| if s == target { Some(p) } else { None }).min()
             {
                 println!("{}", qe);
                 break;
@@ -25,19 +23,17 @@ fn main()
     }
 }
 
-fn combinations(input : &[u64], target_len : usize, partial : Vec<u64>, output : &mut Vec<Vec<u64>>)
+fn combinations(input : &[u64], size : usize, target : u64, sum : u64, product : u64, solutions : &mut Vec<(u64, u64)>)
 {
-    if let 0 = target_len
+    if let 0 = size
     {
-        output.push(partial);
+        solutions.push((sum, product));
     }
-    else
+    else if sum < target
     {
-        for i in 0 ..= input.len() - target_len
+        for i in 0 ..= input.len() - size
         {
-            let mut current = partial.clone();
-            current.push(input[i]);
-            combinations(&input[i+1 ..], target_len-1, current, output);
+            combinations(&input[i+1 ..], size - 1, target, sum + input[i], product * input[i], solutions);
         }
     }
 }
