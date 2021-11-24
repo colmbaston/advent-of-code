0c3c260cd7d5d1de1aaca950473c8a0bdfe349aa
Colm Baston <colm@colmbaston.uk>
2021-11-24 17:33:14 +0000

    2015 day 25

--- 2015/puzzle25/src/main.rs
+++ 2015/puzzle25/src/main.rs
@@ -1,3 +1,25 @@
 fn main()
 {
+    let (row, column) =
+    {
+        fn span(s : &str, p : impl Fn(char) -> bool) -> (&str, &str)
+        {
+            s.split_at(s.find(|c| !p(c)).unwrap_or_else(|| s.len()))
+        }
+
+        let          input  = include_str!("../input.txt");
+        let (_,      input) = span(input, |c| !c.is_ascii_digit());
+        let (row,    input) = span(input, |c|  c.is_ascii_digit());
+        let (_,      input) = span(input, |c| !c.is_ascii_digit());
+        let (column, _    ) = span(input, |c|  c.is_ascii_digit());
+
+        (row.parse::<u32>().unwrap(), column.parse::<u32>().unwrap())
+    };
+
+    let mut code : u64 = 20151125;
+    for _ in 0 .. (row * (row-1) + column * (column-1)) / 2 + row * (column-1)
+    {
+        code = (code * 252533) % 33554393;
+    }
+    println!("{}", code);
 }
