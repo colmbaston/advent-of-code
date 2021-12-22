79ba115f5a14b5ac1c6d5e60f3306fe14c8fd8ae
Colm Baston <colm@colmbaston.uk>
2021-12-22 03:26:25 +0000

    use bounds library to compute min and max

--- 2021/puzzle07/src/main.rs
+++ 2021/puzzle07/src/main.rs
@@ -4,10 +4,12 @@ fn main()
 
     let fuel_one   = |a : i32, b : i32| {         (a - b).abs()                  };
     let fuel_two   = |a : i32, b : i32| { let c = (a - b).abs(); (c * (c+1)) / 2 };
-    let (min, max) = input.iter().fold((i32::MAX, i32::MIN), |(min, max), &k| (min.min(k), max.max(k)));
 
-    for fuel in [fuel_one, fuel_two]
+    if let Some((min, max)) = aoc::bounds::bounds_1d(input.iter())
     {
-        println!("{}", (min ..= max).map(|a| input.iter().map(|&b| fuel(a, b)).sum::<i32>()).min().unwrap());
+        for fuel in [fuel_one, fuel_two]
+        {
+            println!("{}", (min ..= max).map(|a| input.iter().map(|&b| fuel(a, b)).sum::<i32>()).min().unwrap());
+        }
     }
 }
