2df25a68482354212159be70158c36659759c673
Colm Baston <colm@colmbaston.uk>
2022-12-01 05:10:18 +0000

    2022 puzzle 01

--- 2022/puzzle01/src/main.rs
+++ 2022/puzzle01/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    let mut elves = include_str!("../input.txt").split("\n\n")
+                                                .map(|e| e.lines().map(|l| l.parse::<u32>().unwrap()).sum())
+                                                .collect::<Vec<u32>>();
+
+    elves.sort_unstable();
+    println!("{}", elves.iter().last().unwrap());
+    println!("{}", elves.iter().rev().take(3).sum::<u32>());
 }
