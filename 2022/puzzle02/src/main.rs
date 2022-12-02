63e75f3608b91cc93849db8902f90dbe2b54b4fe
Colm Baston <colm@colmbaston.uk>
2022-12-02 16:50:43 +0000

    2022 puzzle 02

--- 2022/puzzle02/src/main.rs
+++ 2022/puzzle02/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    let (one, two) = include_bytes!("../input.txt").chunks(4)
+                                                   .fold((0, 0), |(one, two), arr|
+                                                   {
+                                                       let a = (arr[0] - b'A') as i8;
+                                                       let b = (arr[2] - b'X') as i8;
+                                                       (one + score(a, b)               as u32,
+                                                        two + score(a, (2 + a + b) % 3) as u32)
+                                                   });
+
+    println!("{one}");
+    println!("{two}");
+}
+
+fn score(a : i8, b : i8) -> i8
+{
+    1 + b + ((a - b).rem_euclid(3) * 2 + 1) % 3 * 3
 }
