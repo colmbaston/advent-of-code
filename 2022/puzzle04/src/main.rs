2036e61c950e570104a88d1fb1a5633e9611538d
Colm Baston <colm@colmbaston.uk>
2022-12-04 10:24:30 +0000

    2022 puzzle 04

--- 2022/puzzle04/src/main.rs
+++ 2022/puzzle04/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::ops::RangeInclusive;
+
+fn main()
+{
+    let (one, two) = include_str!("../input.txt").lines()
+                                                 .fold((0, 0), |(one, two), l|
+                                                 {
+                                                     let (mut small, mut large) = parse_ranges(l);
+                                                     if small.len() > large.len() { std::mem::swap(&mut small, &mut large) }
+
+                                                     let contains_start = large.contains(small.start());
+                                                     let contains_end   = large.contains(small.end());
+
+                                                     (one + (contains_start && contains_end) as u32,
+                                                      two + (contains_start || contains_end) as u32)
+                                                 });
+
+    println!("{one}");
+    println!("{two}");
+}
+
+fn parse_ranges(s : &str) -> (RangeInclusive<u8>, RangeInclusive<u8>)
+{
+    let mut nums = s.split(|c : char| !c.is_ascii_digit()).filter_map(|n| n.parse().ok());
+    let first    = nums.next().unwrap() ..= nums.next().unwrap();
+    let second   = nums.next().unwrap() ..= nums.next().unwrap();
+
+    (first, second)
 }
