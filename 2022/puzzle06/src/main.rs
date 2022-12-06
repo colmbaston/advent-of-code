1faa6fe510f1d85d833e163caa2d311aa7633f59
Colm Baston <colm@colmbaston.uk>
2022-12-06 12:29:02 +0000

    2022 puzzle 06

--- 2022/puzzle06/src/main.rs
+++ 2022/puzzle06/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashSet;
+
+fn main()
+{
+    let mut set = HashSet::new();
+    for size in [4, 14]
+    {
+        for (window, ix) in include_str!("../input.txt").trim_end().as_bytes().windows(size).zip(size ..)
+        {
+            set.clear();
+            set.extend(window.iter().copied());
+            if set.len() == window.len() { println!("{ix}"); break }
+        }
+    }
 }
