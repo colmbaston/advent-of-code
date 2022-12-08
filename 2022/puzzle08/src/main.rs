0eeb9f646845add19c6010ba354395ff6e3798c2
Colm Baston <colm@colmbaston.uk>
2022-12-08 18:34:37 +0000

    2022 puzzle 08

--- 2022/puzzle08/src/main.rs
+++ 2022/puzzle08/src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use itertools::Itertools;
+use std::collections::HashSet;
+
+fn main()
+{
+    let trees_by_row = include_str!("../input.txt").lines()
+                                                   .map(|l| l.as_bytes())
+                                                   .collect::<Vec<&[u8]>>();
+    let trees_by_col = aoc::transpose::transpose(&trees_by_row);
+
+    let mut visible = HashSet::new();
+    for (y, row) in trees_by_row.iter().enumerate()
+    {
+        let row_iter = row.iter().copied().enumerate();
+        visible.extend(       visible_indices(row_iter.clone())
+                       .chain(visible_indices(row_iter.rev()))
+                       .map(|x| (x, y)));
+    }
+    for (x, col) in trees_by_col.iter().enumerate()
+    {
+        let col_iter = col.iter().copied().enumerate();
+        visible.extend(       visible_indices(col_iter.clone())
+                       .chain(visible_indices(col_iter.rev()))
+                       .map(|y| (x, y)));
+    }
+    println!("{}", visible.len());
+
+    let mut score = 0;
+    for (y, row) in trees_by_row.iter().enumerate().skip(1).take(trees_by_row.len()-2)
+    {
+        for ((x, col), &height) in trees_by_col.iter().enumerate().skip(1).take(trees_by_col.len()-2).zip(row.iter().skip(1))
+        {
+            score = score.max(viewing_distance(height, col[..   y].iter().copied().rev())
+                            * viewing_distance(height, col[y+1 ..].iter().copied())
+                            * viewing_distance(height, row[..   x].iter().copied().rev())
+                            * viewing_distance(height, row[x+1 ..].iter().copied()));
+        }
+    }
+    println!("{score}");
+}
+
+fn visible_indices(iter : impl Iterator<Item = (usize, u8)>) -> impl Iterator<Item = usize>
+{
+    let mut max = 0;
+    iter.filter_map(move |(i, b)|
+    {
+        if b > max { max = b; Some(i) } else { None }
+    })
+}
+
+fn viewing_distance(height : u8, iter : impl Iterator<Item = u8>) -> usize
+{
+    let mut iter = iter.peekable();
+    iter.peeking_take_while(|&h| h < height).count() + iter.next().is_some() as usize
 }
--- aoc/src/transpose.rs
+++ aoc/src/transpose.rs
@@ -1,6 +1,6 @@
-pub fn transpose<T : Clone>(matrix : &[Vec<T>]) -> Vec<Vec<T>>
+pub fn transpose<T : Clone>(matrix : &[&[T]]) -> Vec<Vec<T>>
 {
-    let num_cols = matrix.get(0).map(Vec::len).unwrap_or(0);
+    let num_cols = matrix.get(0).map(|row| row.len()).unwrap_or(0);
     let mut rows = matrix.iter().map(|row| row.iter().cloned()).collect::<Vec<_>>();
 
     (0 .. num_cols).map(|_| rows.iter_mut()
