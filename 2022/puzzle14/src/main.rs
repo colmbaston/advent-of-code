e6846b0afdd867f52e18b45cb6dcdeb85b2ab1df
Colm Baston <colm@colmbaston.uk>
2022-12-14 21:47:06 +0000

    fine, I'll use an array

--- 2022/puzzle14/src/main.rs
+++ 2022/puzzle14/src/main.rs
@@ -1,37 +1,45 @@
 use std::{ collections::HashSet, cmp::Ordering };
 
-type Pos = (i16, i16);
+type Pos = (usize, usize);
 
 fn main()
 {
-    let mut blocked  = parse_clay(include_str!("../input.txt"));
-    let clay_len     = blocked.len();
-    let max_y        = 1 + blocked.iter().map(|&(_, y)| y).max().unwrap_or(0);
+    let clay  = parse_clay(include_str!("../input.txt"));
+    let max_y = 1 + clay.iter().map(|&(_, y)| y).max().unwrap_or(0);
+
+    let mut grid = vec![vec![false ; max_y+1] ; 2*max_y+1];
+    for (x, y) in clay.into_iter()
+    {
+        if let Some(x) = (x + max_y).checked_sub(500) { grid[x][y] = true }
+    }
+
+    let mut sand     = 0;
     let mut part_one = true;
-    while let Some(pos@(_, y)) = drop_sand((500, 0), |pos@(_, y)| y > max_y || blocked.contains(&pos))
+    while let Some((x, y)) = drop_sand((max_y, 0), |(x, y)| y > max_y || grid[x][y])
     {
         if part_one && y == max_y
         {
-            println!("{}", blocked.len() - clay_len);
+            println!("{sand}");
             part_one = false;
         }
-        blocked.insert(pos);
+        grid[x][y] = true;
+        sand += 1;
     }
-    println!("{}", blocked.len() - clay_len);
+    println!("{sand}");
 }
 
 fn drop_sand((sx, sy) : Pos, blocked : impl Fn(Pos) -> bool + Copy) -> Option<Pos>
 {
     (sy ..).take_while(|y| !blocked((sx, *y)))
-           .last().map(|y|            drop_sand((sx-1, y+1), blocked)
-                          .or_else(|| drop_sand((sx+1, y+1), blocked))
-                          .unwrap_or((sx, y)))
+           .last().map(|y| sx.checked_sub(1).and_then(|x| drop_sand((x,    y+1), blocked))
+                                            .or_else(||   drop_sand((sx+1, y+1), blocked))
+                                            .unwrap_or((sx, y)))
 }
 
 fn parse_clay(s : &str) -> HashSet<Pos>
 {
-    let mut buff  = Vec::new();
-    let mut walls = HashSet::new();
+    let mut buff = Vec::new();
+    let mut clay = HashSet::new();
 
     for line in s.lines()
     {
@@ -39,8 +47,8 @@ fn parse_clay(s : &str) -> HashSet<Pos>
         buff.extend(line.split(" -> ").filter_map(|word|
         {
             let mut digits = word.split(',');
-            digits.next().and_then(|x| x.parse::<i16>().ok())
-                         .and_then(|x| digits.next().and_then(|y| y.parse::<i16>().ok().map(|y| (x, y))))
+            digits.next().and_then(|x| x.parse::<usize>().ok())
+                         .and_then(|x| digits.next().and_then(|y| y.parse::<usize>().ok().map(|y| (x, y))))
         }));
 
         for w in buff.windows(2)
@@ -50,14 +58,14 @@ fn parse_clay(s : &str) -> HashSet<Pos>
 
             match (ax.cmp(&bx), ay.cmp(&by))
             {
-                (Ordering::Equal, Ordering::Less ) => walls.extend((ay ..= by).map(|y| (ax, y))),
-                (Ordering::Equal, _              ) => walls.extend((by ..= ay).map(|y| (ax, y))),
-                (Ordering::Less,  Ordering::Equal) => walls.extend((ax ..= bx).map(|x| (x, ay))),
-                (_,               Ordering::Equal) => walls.extend((bx ..= ax).map(|x| (x, ay))),
+                (Ordering::Equal, Ordering::Less ) => clay.extend((ay ..= by).map(|y| (ax, y))),
+                (Ordering::Equal, _              ) => clay.extend((by ..= ay).map(|y| (ax, y))),
+                (Ordering::Less,  Ordering::Equal) => clay.extend((ax ..= bx).map(|x| (x, ay))),
+                (_,               Ordering::Equal) => clay.extend((bx ..= ax).map(|x| (x, ay))),
                 _                                  => unreachable!()
             }
         }
     }
 
-    walls
+    clay
 }
