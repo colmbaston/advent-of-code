1d26597963d7c579d048b0c6f13c821fc941f3af
Colm Baston <colm@colmbaston.uk>
2022-12-14 18:10:09 +0000

    merged clay and sand sets

--- 2022/puzzle14/src/main.rs
+++ 2022/puzzle14/src/main.rs
@@ -1,51 +1,48 @@
 use std::{ collections::HashSet, cmp::Ordering };
 
-type Pos = (u32, u32);
-const SOURCE : Pos = (500, 0);
+type Pos = (i16, i16);
 
 fn main()
 {
-    let walls = parse_walls(include_str!("../input.txt"));
-    let max_y = 1 + walls.iter().map(|(_, y)| y).copied().max().unwrap_or(0);
-
-    let mut sand     = HashSet::new();
+    let mut blocked  = parse_clay(include_str!("../input.txt"));
+    let clay_len     = blocked.len();
+    let max_y        = 1 + blocked.iter().map(|&(_, y)| y).max().unwrap_or(0);
     let mut part_one = true;
-    while let Some(pos@(_, y)) = drop_sand(SOURCE, max_y, |p| walls.contains(p) || sand.contains(p))
+    while let Some(pos@(_, y)) = drop_sand((500, 0), |pos@(_, y)| y > max_y || blocked.contains(&pos))
     {
         if part_one && y == max_y
         {
-            println!("{}", sand.len());
+            println!("{}", blocked.len() - clay_len);
             part_one = false;
         }
-        sand.insert(pos);
+        blocked.insert(pos);
     }
-    println!("{}", sand.len());
+    println!("{}", blocked.len() - clay_len);
 }
 
-fn drop_sand((sx, sy) : Pos, max_y : u32, blocked : impl Fn(&Pos) -> bool + Copy) -> Option<Pos>
+fn drop_sand((sx, sy) : Pos, blocked : impl Fn(Pos) -> bool + Copy) -> Option<Pos>
 {
-    (sy ..).map(|y| (sx, y))
-           .take_while(|p| p.1 <= max_y && !blocked(p))
-           .last().and_then(|(x, y)| drop_sand((x-1, y+1), max_y, blocked)
-                         .or_else(|| drop_sand((x+1, y+1), max_y, blocked))
-                         .or(Some((x, y))))
+    (sy ..).take_while(|y| !blocked((sx, *y)))
+           .last().map(|y|            drop_sand((sx-1, y+1), blocked)
+                          .or_else(|| drop_sand((sx+1, y+1), blocked))
+                          .unwrap_or((sx, y)))
 }
 
-fn parse_walls(s : &str) -> HashSet<Pos>
+fn parse_clay(s : &str) -> HashSet<Pos>
 {
     let mut buff  = Vec::new();
     let mut walls = HashSet::new();
 
-    s.lines().map(|line| line.split(" -> ").filter_map(|word|
-    {
-        let mut digits = word.split(',');
-        digits.next().and_then(|x| x.parse::<u32>().ok())
-                     .and_then(|x| digits.next().and_then(|y| y.parse::<u32>().ok().map(|y| (x, y))))
-    }))
-    .for_each(|line|
+    for line in s.lines()
     {
         buff.clear();
-        buff.extend(line);
+        buff.extend(line.split(" -> ").filter_map(|word|
+        {
+            let mut digits = word.split(',');
+            digits.next().and_then(|x| x.parse::<i16>().ok())
+                         .and_then(|x| digits.next().and_then(|y| y.parse::<i16>().ok().map(|y| (x, y))))
+        }));
+
         for w in buff.windows(2)
         {
             let (ax, ay) = w[0];
@@ -60,7 +57,7 @@ fn parse_walls(s : &str) -> HashSet<Pos>
                 _                                  => unreachable!()
             }
         }
-    });
+    }
 
     walls
 }
