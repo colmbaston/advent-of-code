cbe9428a4de1e4cdf67786c5a74a72c489b4524c
Colm Baston <colm@colmbaston.uk>
2022-12-15 17:50:46 +0000

    bring part one outside the part two loop

--- 2022/puzzle15/src/main.rs
+++ 2022/puzzle15/src/main.rs
@@ -5,74 +5,50 @@ use sensor::Sensor;
 
 fn main()
 {
-    let (sensors, beacons) = include_str!("../input.txt").lines().fold((Vec::new(), HashSet::new()), |(mut sensors, mut beacons), l|
+    let (sensors, beacons) = include_str!("../input.txt").lines().fold((Vec::new(), HashSet::new()), |(mut ss, mut bs), l|
     {
-        if let Some((sensor, beacon)) = Sensor::parse(l)
-        {
-            sensors.push(sensor);
-            beacons.insert(beacon);
-        }
-        (sensors, beacons)
+        if let Some((s, b)) = Sensor::parse(l) { ss.push(s); bs.insert(b); }
+        (ss, bs)
     });
 
-    let mut one      = None;
-    let mut two      = None;
     let mut coverage = Vec::new();
     let mut merged   = Vec::new();
-    let distress_x   = 0 ..= 4_000_000;
-    let distress_y   = 0 ..= 4_000_000;
-    for y in distress_y
+
+    merge_coverage(2_000_000, sensors.iter(), &mut coverage, &mut merged);
+    let covered = merged.iter().map(|r| if r.is_empty() { 0 } else { 1 + (*r.end() - *r.start()) as usize }).sum::<usize>();
+    let beacons = beacons.into_iter().filter(|b| b.y == 2_000_000).count();
+    println!("{}", covered - beacons);
+
+    'outer: for y in 0 ..= 4_000_000
     {
-        coverage.extend(sensors.iter().filter_map(|s| { let c = s.coverage_y(y); (!c.is_empty()).then_some(c) }));
-        coverage.sort_unstable_by_key(|r| *r.start());
-        merged.clear();
-        merge_ranges(coverage.drain(..), &mut merged);
-
-        if y == 2_000_000
-        {
-            let covered = merged.iter().map(range_size).sum::<usize>();
-            let beacons = beacons.iter().filter(|b| b.y == y).count();
-            one         = Some(covered - beacons);
-        }
-
+        merge_coverage(y, sensors.iter(), &mut coverage, &mut merged);
         for gap in merged.windows(2).map(|w| *w[0].end()+1 ..= *w[1].start()-1)
         {
-            let x = gap.end();
-            if distress_x.contains(x)
+            let x = gap.start();
+            if x == gap.end() && (0 ..= 4_000_000).contains(x)
             {
-                two = Some(*x as usize * 4_000_000 + y as usize);
+                println!("{}", *x as u64 * 4_000_000 + y as u64);
+                break 'outer
             }
-        }
-
-        if let Some((one, two)) = one.and_then(|one| two.map(|two| (one, two)))
-        {
-            println!("{one}");
-            println!("{two}");
-            break
         }
     }
 }
 
-fn merge_ranges(ranges : impl Iterator<Item = RangeInclusive<i32>>, merged : &mut Vec<RangeInclusive<i32>>)
+fn merge_coverage<'a>(row : i32, sensors : impl Iterator<Item = &'a Sensor>, coverage : &mut Vec<RangeInclusive<i32>>, merged : &mut Vec<RangeInclusive<i32>>)
 {
-    for range in ranges
+    coverage.extend(sensors.filter_map(|s| { let r = s.row_coverage(row); (!r.is_empty()).then_some(r) }));
+    coverage.sort_unstable_by_key(|r| *r.start());
+
+    merged.clear();
+    for range in coverage.drain(..)
     {
         match merged.last_mut()
         {
-            None       => merged.push(range),
-            Some(last) => if *range.start() <= *last.end()+1
+            Some(last) if *range.start() <= *last.end()+1 =>
             {
                 *last = *last.start() ..= *last.end().max(range.end())
-            }
-            else
-            {
-                merged.push(range);
-            }
+            },
+            _ => merged.push(range)
         }
     }
-}
-
-fn range_size(range : &RangeInclusive<i32>) -> usize
-{
-    if range.is_empty() { 0 } else { 1 + (range.end() - range.start()) as usize }
 }
--- 2022/puzzle15/src/sensor.rs
+++ 2022/puzzle15/src/sensor.rs
@@ -1,10 +1,36 @@
 use std::ops::RangeInclusive;
 
+pub struct Sensor
+{
+    pos:   Pos,
+    range: u32
+}
+
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Pos
 {
     pub x: i32,
     pub y: i32
+}
+
+impl Sensor
+{
+    pub fn parse(s : &str) -> Option<(Sensor, Pos)>
+    {
+        let s        = s.strip_prefix("Sensor at ")?;
+        let (pos, s) = s.split_at(s.find(':')?);
+        let beacon   = s.strip_prefix(": closest beacon is at ")?;
+
+        let pos    = Pos::parse(pos)?;
+        let beacon = Pos::parse(beacon)?;
+        Some((Sensor { pos, range: pos.manhattan(&beacon) }, beacon))
+    }
+
+    pub fn row_coverage(&self, row : i32) -> RangeInclusive<i32>
+    {
+        let diff = self.range as i32 - row.abs_diff(self.pos.y) as i32;
+        self.pos.x - diff ..= self.pos.x + diff
+    }
 }
 
 impl Pos
@@ -22,31 +48,5 @@ impl Pos
     fn manhattan(&self, other : &Pos) -> u32
     {
         self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
-    }
-}
-
-pub struct Sensor
-{
-    pos:   Pos,
-    range: u32
-}
-
-impl Sensor
-{
-    pub fn parse(s : &str) -> Option<(Sensor, Pos)>
-    {
-        let s        = s.strip_prefix("Sensor at ")?;
-        let (pos, s) = s.split_at(s.find(':')?);
-        let beacon   = s.strip_prefix(": closest beacon is at ")?;
-
-        let pos    = Pos::parse(pos)?;
-        let beacon = Pos::parse(beacon)?;
-        Some((Sensor { pos, range: pos.manhattan(&beacon) }, beacon))
-    }
-
-    pub fn coverage_y(&self, y : i32) -> RangeInclusive<i32>
-    {
-        let diff_y = y.abs_diff(self.pos.y) as i32;
-        self.pos.x - self.range as i32 + diff_y ..= self.pos.x + self.range as i32 - diff_y
     }
 }
