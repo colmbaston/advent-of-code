c3885073a6078b62c94b526f1a917086d88aed8c
Colm Baston <colm@colmbaston.uk>
2022-12-16 18:44:12 +0000

    2022 puzzle 16, part one

--- 2022/puzzle16/src/main.rs
+++ 2022/puzzle16/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{ HashMap, HashSet, VecDeque };
 
 fn main()
 {
@@ -10,8 +10,20 @@ fn main()
         tunnels.insert(valve, ts);
     }
 
-    println!("{rates:?}");
-    println!("{tunnels:?}");
+    let mut visited = HashSet::new();
+    let mut queue   = VecDeque::new();
+    queue.push_back(State { valve: "AA", minute: 1, pressure: 0, rates });
+
+    let mut max = 0;
+    while let Some(state) = queue.pop_front()
+    {
+        if state.minute == 30
+        || !visited.insert((state.valve, state.pressure)) { continue }
+
+        max = max.max(state.pressure);
+        queue.extend(state.step(&tunnels));
+    }
+    println!("{max}");
 }
 
 fn parse_valve(s : &str) -> Option<(&str, u32, Vec<&str>)>
@@ -24,4 +36,42 @@ fn parse_valve(s : &str) -> Option<(&str, u32, Vec<&str>)>
                      s.strip_prefix("; tunnel leads to valve "))?;
 
     Some((valve, rate.parse().ok()?, s.split(", ").collect()))
+}
+
+struct State<'a>
+{
+    valve:    &'a str,
+    minute:   u32,
+    pressure: u32,
+    rates:    HashMap<&'a str, u32>
+}
+
+impl<'a> State<'a>
+{
+    fn step<'b>(&'b self, tunnels : &'a HashMap<&str, Vec<&str>>) -> impl Iterator<Item = State<'a>> + 'b
+    {
+        let open_valve = self.rates.get(self.valve).into_iter().map(|&rate|
+        {
+            let mut rates = self.rates.clone();
+            rates.remove(self.valve);
+
+            State
+            {
+                valve:    self.valve,
+                minute:   self.minute+1,
+                pressure: self.pressure + rate * (30 - self.minute),
+                rates
+            }
+        });
+
+        let tunnel = tunnels.get(self.valve).into_iter().flat_map(|ts| ts.iter().map(|valve| State
+        {
+            valve,
+            minute:    self.minute+1,
+            pressure:  self.pressure,
+            rates:     self.rates.clone()
+        }));
+
+        open_valve.chain(tunnel)
+    }
 }
