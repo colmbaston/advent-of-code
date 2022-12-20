7b0a9a3c6f31a456515be6a1087e70554d87397c
Colm Baston <colm@colmbaston.uk>
2022-12-20 19:30:39 +0000

    cleaned up part two

--- 2022/puzzle19/src/inventory.rs
+++ 2022/puzzle19/src/inventory.rs
@@ -1,18 +1,4 @@
-use std::ops::{ Sub, Index, IndexMut };
-
-#[derive(Clone, Copy)]
-pub enum Resource { Ore, Clay, Obsidian, Geode }
-
-impl Resource
-{
-    pub fn iter() -> impl Iterator<Item = Resource>
-    {
-        [Resource::Ore,
-         Resource::Clay,
-         Resource::Obsidian,
-         Resource::Geode].into_iter()
-    }
-}
+use std::ops::{ Index, IndexMut };
 
 #[derive(Clone, PartialEq, Eq, Hash)]
 pub struct Inventory
@@ -37,49 +23,15 @@ impl Inventory
             geode:    self.geode.checked_sub(other.geode)?
         })
     }
-}
 
-impl Sub for &Inventory
-{
-    type Output = Inventory;
-
-    fn sub(self, other : &Inventory) -> Inventory
+    pub fn max(&self, other : &Inventory) -> Inventory
     {
-        let mut inventory = self.clone();
-        for resource in Resource::iter()
+        Inventory
         {
-            inventory[resource] -= other[resource]
-        }
-        inventory
-    }
-}
-
-impl Index<Resource> for Inventory
-{
-    type Output = u16;
-
-    fn index(&self, resource : Resource) -> &u16
-    {
-        match resource
-        {
-            Resource::Ore      => &self.ore,
-            Resource::Clay     => &self.clay,
-            Resource::Obsidian => &self.obsidian,
-            Resource::Geode    => &self.geode
-        }
-    }
-}
-
-impl IndexMut<Resource> for Inventory
-{
-    fn index_mut(&mut self, resource : Resource) -> &mut u16
-    {
-        match resource
-        {
-            Resource::Ore      => &mut self.ore,
-            Resource::Clay     => &mut self.clay,
-            Resource::Obsidian => &mut self.obsidian,
-            Resource::Geode    => &mut self.geode
+            ore:      self.ore.max(other.ore),
+            clay:     self.clay.max(other.clay),
+            obsidian: self.obsidian.max(other.obsidian),
+            geode:    self.geode.max(other.geode)
         }
     }
 }
@@ -114,7 +66,7 @@ impl Blueprint
         let s       = s.strip_prefix(':')?;
 
         let mut blueprint = Blueprint::empty(id.parse().ok()?);
-        for (resource, mut words) in Resource::iter().zip(s.split('.').map(|s| s.split_whitespace()))
+        for (resource, mut words) in Resource::enumerate().zip(s.split('.').map(|s| s.split_whitespace()))
         {
             let inventory = &mut blueprint[resource];
             inventory[Resource::Ore] = words.nth(4)?.parse().ok()?;
@@ -128,6 +80,50 @@ impl Blueprint
         }
 
         Some(blueprint)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub enum Resource { Ore, Clay, Obsidian, Geode }
+
+impl Resource
+{
+    pub fn enumerate() -> impl Iterator<Item = Resource>
+    {
+        [Resource::Ore,
+         Resource::Clay,
+         Resource::Obsidian,
+         Resource::Geode].into_iter()
+    }
+}
+
+impl Index<Resource> for Inventory
+{
+    type Output = u16;
+
+    fn index(&self, resource : Resource) -> &u16
+    {
+        match resource
+        {
+            Resource::Ore      => &self.ore,
+            Resource::Clay     => &self.clay,
+            Resource::Obsidian => &self.obsidian,
+            Resource::Geode    => &self.geode
+        }
+    }
+}
+
+impl IndexMut<Resource> for Inventory
+{
+    fn index_mut(&mut self, resource : Resource) -> &mut u16
+    {
+        match resource
+        {
+            Resource::Ore      => &mut self.ore,
+            Resource::Clay     => &mut self.clay,
+            Resource::Obsidian => &mut self.obsidian,
+            Resource::Geode    => &mut self.geode
+        }
     }
 }
 
--- 2022/puzzle19/src/main.rs
+++ 2022/puzzle19/src/main.rs
@@ -1,8 +1,7 @@
 use std::collections::HashSet;
 
-mod resource;
-use resource::{ Resource, Inventory, Blueprint };
-
+mod inventory;
+use inventory::{ Inventory, Blueprint, Resource };
 
 fn main()
 {
@@ -14,44 +13,45 @@ fn main()
     let mut buffer  = Vec::new();
     let mut visited = HashSet::new();
 
-    let mut sum = 0;
-    for blueprint in blueprints.iter()
+    println!("{}", blueprints.iter().map(|b| b.id *  explore(b, 24, &mut queue, &mut buffer, &mut visited)).sum::<u16>());
+    println!("{}", blueprints.iter().take(3).map(|b| explore(b, 32, &mut queue, &mut buffer, &mut visited)).product::<u16>());
+}
+
+fn explore(blueprint : &Blueprint, minutes : u16, queue : &mut Vec<State>, buffer : &mut Vec<State>, visited : &mut HashSet<State>) -> u16
+{
+    queue.push(State::init(minutes));
+    visited.clear();
+
+    let cost_max = Resource::enumerate().fold(Inventory::EMPTY, |acc, resource| acc.max(&blueprint[resource]));
+
+    let mut geode_max = 0;
+    while let Some(state) = queue.pop()
     {
-        queue.push(State::init(24));
-        visited.clear();
-
-        let mut geodes = 0;
-        while let Some(state) = queue.pop()
+        if state.minutes == 0
         {
-            if state.minutes == 0
-            {
-                geodes = geodes.max(state.resources[Resource::Geode]);
-                continue
-            }
-
-            if !visited.insert(state.clone()) { continue }
-            state.step(blueprint, &mut buffer);
-            queue.extend(buffer.drain(..).filter(|next| !next.prune(geodes, blueprint, &state)));
+            geode_max = geode_max.max(state.resources[Resource::Geode]);
+            continue
         }
 
-        let quality = geodes * blueprint.id;
-        sum += quality;
-        println!("blueprint {}: cracked {geodes} geodes, quality {quality}", blueprint.id);
+        if !visited.insert(state.clone()) { continue }
+
+        state.step(blueprint, buffer);
+        queue.extend(buffer.drain(..).filter(|next| !next.prune(geode_max, &cost_max)));
     }
-    println!("{sum}");
+    geode_max
 }
 
 #[derive(Clone, PartialEq, Eq, Hash)]
 struct State
 {
-    minutes:   u8,
+    minutes:   u16,
     resources: Inventory,
     robots:    Inventory
 }
 
 impl State
 {
-    fn init(minutes : u8) -> State
+    fn init(minutes : u16) -> State
     {
         let mut state = State
         {
@@ -67,7 +67,7 @@ impl State
     fn step(&self, blueprint : &Blueprint, buffer : &mut Vec<State>)
     {
         buffer.push(self.clone());
-        buffer.extend(Resource::iter().filter_map(|resource|
+        buffer.extend(Resource::enumerate().filter_map(|resource|
         {
             let resources  = self.resources.checked_sub(&blueprint[resource])?;
             let mut robots = self.robots.clone();
@@ -78,15 +78,31 @@ impl State
         for state in buffer.iter_mut()
         {
             state.minutes -= 1;
-            for resource in Resource::iter()
+            for resource in Resource::enumerate()
             {
                 state.resources[resource] += self.robots[resource];
             }
         }
     }
 
-    fn prune(&self, _geodes : u16, _blueprint : &Blueprint, _prev : &State) -> bool
+    fn prune(&self, geode_max : u16, cost_max : &Inventory) -> bool
     {
-        false
+        self.geode_upper_bound() <= geode_max ||
+        self.redundant_robots(cost_max)
+    }
+
+    fn geode_upper_bound(&self) -> u16
+    {
+        self.resources[Resource::Geode] +
+        self.minutes * self.robots[Resource::Geode] +
+        self.minutes * self.minutes.saturating_sub(1) / 2
+    }
+
+    fn redundant_robots(&self, cost_max : &Inventory) -> bool
+    {
+        Resource::enumerate().take(3).any(|resource|
+        {
+            self.robots[resource] > cost_max[resource]
+        })
     }
 }
