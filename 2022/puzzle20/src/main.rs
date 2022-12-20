2ed6199a66b8d35d651a6464c428627bbfc4ef9a
Colm Baston <colm@colmbaston.uk>
2022-12-20 16:52:46 +0000

    inline rotate function

--- 2022/puzzle20/src/main.rs
+++ 2022/puzzle20/src/main.rs
@@ -2,39 +2,30 @@ use std::collections::VecDeque;
 
 fn main()
 {
-    let mut file   = include_str!("../input.txt").lines().filter_map(|l| l.parse().ok()).collect::<Vec<i64>>();
+    let file       = include_str!("../input.txt").lines().filter_map(|l| l.parse().ok()).collect::<Vec<i64>>();
     let mut buffer = VecDeque::new();
 
-    for two in [false, true]
+    for (key, rounds) in [(1, 1), (811_589_153, 10)]
     {
-        if two { for val in file.iter_mut() { *val *= 811_589_153 }}
-
         buffer.clear();
         buffer.extend(0 .. file.len());
 
-        for _ in 0 .. if two { 10 } else { 1 }
-        {
-            mix(&file, &mut buffer);
-        }
+        for _ in 0 .. rounds { mix(&file, key, &mut buffer) }
 
         let zero = buffer.iter().position(|&i| file[i] == 0).unwrap_or(0);
-        println!("{}", (1 ..= 3).map(|i| file[buffer[(zero + i*1000) % file.len()]]).sum::<i64>());
+        println!("{}", (1 ..= 3).map(|i| key * file[buffer[(zero + i*1000) % file.len()]]).sum::<i64>());
     }
 }
 
-fn rotate<T>(buffer : &mut VecDeque<T>, rot : i64)
+fn mix(file : &[i64], key : i64, buffer : &mut VecDeque<usize>)
 {
-    let fun = if rot < 0 { VecDeque::rotate_right } else { VecDeque::rotate_left };
-    fun(buffer, rot.unsigned_abs() as usize % buffer.len());
-}
-
-fn mix(file : &[i64], buffer : &mut VecDeque<usize>)
-{
-    for i in 0 .. file.len()
+    for (i, val) in file.iter().map(|val| key * val).enumerate()
     {
-        rotate(buffer, buffer.iter().position(|&j| j == i).unwrap_or(0) as i64);
+        buffer.rotate_left(buffer.iter().position(|&j| j == i).unwrap_or(0));
         buffer.pop_front();
-        rotate(buffer, file[i]);
+
+        let rot = val.unsigned_abs() as usize % buffer.len();
+        if val.is_positive() { buffer.rotate_left(rot) } else { buffer.rotate_right(rot) }
         buffer.push_front(i);
     }
 }
