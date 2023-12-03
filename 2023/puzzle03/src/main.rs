be320055e4127dd487c17462ef4b579e5d5097c8
Colm Baston <colm@colmbaston.uk>
2023-12-03 12:14:06 +0000

    improve adjacency checking

--- 2023/puzzle03/src/main.rs
+++ 2023/puzzle03/src/main.rs
@@ -9,42 +9,45 @@ fn main()
     for (row, y) in include_str!("../input.txt").lines().map(str::as_bytes).zip(0..)
     {
         let mut x = 0;
-        while let Some(&b) = row.get(x)
+        while let Some(&b) = row.get(x as usize)
         {
             if b.is_ascii_digit()
             {
-                let num = row[x..].split(|b| !b.is_ascii_digit()).next().unwrap();
-                parts.push(Part
-                {
-                    number: num.iter().fold(0, |a, k| 10 * a + (k - b'0') as u32),
-                    pos:    (x as i32, y),
-                    len:    num.len()
-                });
-                x += num.len();
+                let digits = row[x as usize ..].split(|b| !b.is_ascii_digit()).next().unwrap();
+
+                let number = digits.iter().fold(0, |a, k| 10 * a + (k - b'0') as u32);
+                let pos    = (x, y);
+                let len    = digits.len() as i32;
+
+                parts.push(Part { number, pos, len });
+                x += len;
             }
             else
             {
-                if b != b'.' { symbols.insert((x as i32, y), b); }
+                if b != b'.' { symbols.insert((x, y), b); }
                 x += 1
             }
         }
     }
 
     println!("{}", parts.iter()
-                        .filter_map(|part| part.surrounding()
+                        .filter_map(|part| part.adjacents()
                                                .any(|pos| symbols.contains_key(&pos))
                                                .then_some(part.number))
                         .sum::<u32>());
 
-    symbols.retain(|_, b : &mut u8| *b == b'*');
-    println!("{}", symbols.into_keys().filter_map(|pos|
+    println!("{}", symbols.into_iter().filter_map(|(pos, symb)|
     {
-        let mut ps = parts.iter()
-                          .filter(|part| adjacent(pos).any(|p| part.covers(p)));
+        (symb == b'*').then(||
+        {
+            let mut ps = parts.iter()
+                              .filter(|part| part.is_adjacent(pos));
 
-        ps.next_chunk().ok()
-          .and_then(|[p1, p2]| ps.next().is_none()
-                                 .then_some(p1.number * p2.number))
+            ps.next_chunk().ok()
+              .and_then(|[p1, p2]| ps.next().is_none()
+                                     .then_some(p1.number * p2.number))
+        })
+        .flatten()
     })
     .sum::<u32>());
 }
@@ -55,32 +58,24 @@ struct Part
 {
     number: u32,
     pos:    Pos,
-    len:    usize
-}
-
-fn adjacent((x, y) : Pos) -> impl Iterator<Item = Pos>
-{
-    (x-1 ..= x+1).map(move |x| (x, y-1)).chain((x-1 ..= x+1).map(move |x| (x, y+1)))
-                                        .chain(std::iter::once((x-1, y)))
-                                        .chain(std::iter::once((x+1, y)))
+    len:    i32
 }
 
 impl Part
 {
-    fn surrounding(&self) -> impl Iterator<Item = Pos>
+    fn adjacents(&self) -> impl Iterator<Item = Pos>
     {
         let (x, y) = self.pos;
-        let len    = self.len as i32;
 
-        (x-1 ..= x+len).map(move |x| (x, y-1)).chain((x-1 ..= x+len).map(move |x| (x, y+1)))
-                                              .chain(std::iter::once((x-1,   y)))
-                                              .chain(std::iter::once((x+len, y)))
+        (x-1 ..= x+self.len).map(move |x| (x, y-1)).chain((x-1 ..= x+self.len).map(move |x| (x, y+1)))
+                                                   .chain(std::iter::once((x-1,   y)))
+                                                   .chain(std::iter::once((x+self.len, y)))
     }
 
-    fn covers(&self, (x, y) : Pos) -> bool
+    fn is_adjacent(&self, (x, y) : Pos) -> bool
     {
         let (px, py) = self.pos;
 
-        y == py && px <= x && x < px + self.len as i32
+        (y == py-1 || y == py+1) && px-1 <= x && x <= px+self.len || y == py && (x == px-1 || x == px+self.len)
     }
 }
