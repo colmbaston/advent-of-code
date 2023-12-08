3c8251be4a597707a35c699673ff260360d08204
Colm Baston <colm@colmbaston.uk>
2023-12-08 18:26:46 +0000

    improved algorithm for part two

--- 2023/puzzle05/src/main.rs
+++ 2023/puzzle05/src/main.rs
@@ -1,5 +1,5 @@
 #![feature(slice_as_chunks, iter_next_chunk)]
-use std::ops::Range;
+use std::ops::{ Range, Neg };
 
 fn main()
 {
@@ -9,24 +9,31 @@ fn main()
                            .split_whitespace()
                            .map(|k| k.parse::<i64>().unwrap())
                            .collect::<Vec<i64>>();
-    let mut almenac = Almenac(input.map(Map::parse).collect::<Vec<Map>>());
+    let mut almenac = Almenac(input.map(Map::parse)
+                                   .collect::<Vec<Map>>());
 
     println!("{}", seeds.iter()
-                        .map(|&seed| almenac.apply(seed))
+                        .map(|&s| almenac.apply(s))
                         .min().unwrap());
 
-    almenac.invert();
     let seeds = seeds.as_chunks().0.iter()
                      .map(|&[s1, s2]| s1 .. s1+s2)
                      .collect::<Vec<Range<i64>>>();
 
-    println!("{}", (0 ..).find(|&loc|
+    almenac.invert();
+    let mut endpoints = Vec::new();
+    for m in almenac.0.iter()
     {
+        endpoints.iter_mut().for_each(|e| *e = m.apply(*e));
+        endpoints.extend(m.endpoints());
+    }
 
-        let seed = almenac.apply(loc);
-        seeds.iter().any(|r| r.contains(&seed))
-    })
-    .unwrap());
+    almenac.invert();
+    println!("{}", endpoints.into_iter()
+                            .filter(|e| seeds.iter().any(|s| s.contains(e)))
+                            .chain(seeds.iter().map(|s| s.start))
+                            .map(|s| almenac.apply(s))
+                            .min().unwrap())
 }
 
 struct Almenac(Vec<Map>);
@@ -35,13 +42,13 @@ impl Almenac
 {
     fn apply(&self, n : i64) -> i64
     {
-        self.0.iter().fold(n, |a, map| map.apply(a))
+        self.0.iter().fold(n, |a, m| m.apply(a))
     }
 
     fn invert(&mut self)
     {
         self.0.reverse();
-        self.0.iter_mut().for_each(Map::invert);
+        self.0.iter_mut().for_each(Map::invert)
     }
 }
 
@@ -57,8 +64,14 @@ impl Map
 
     fn invert(&mut self)
     {
-        self.0.reverse();
         self.0.iter_mut().for_each(Mapping::invert)
+    }
+
+    fn endpoints(&self) -> impl Iterator<Item = i64> + '_
+    {
+        std::iter::once(0).chain(self.0.iter()
+                                       .flat_map(|m| [m.range.start + m.offset,
+                                                      m.range.end   + m.offset]))
     }
 
     fn parse(s : &str) -> Map
@@ -77,14 +90,13 @@ impl Mapping
 {
     fn apply(&self, n : i64) -> Result<i64, i64>
     {
-        (!self.range.contains(&n)).then_some(n)
-                                  .ok_or(n + self.offset)
+        (!self.range.contains(&n)).then_some(n).ok_or(n + self.offset)
     }
 
     fn invert(&mut self)
     {
         self.range  = self.range.start + self.offset .. self.range.end + self.offset;
-        self.offset = -self.offset;
+        self.offset = self.offset.neg()
     }
 
     fn parse(s : &str) -> Mapping
@@ -93,10 +105,6 @@ impl Mapping
                                    .map(|k| k.parse::<i64>().unwrap())
                                    .next_chunk().unwrap();
 
-        Mapping
-        {
-            range:  source .. source+len,
-            offset: dest - source
-        }
+        Mapping { range:  source .. source+len, offset: dest - source }
     }
 }
