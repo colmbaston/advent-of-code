ba0f38d973127191a1c94f215a7158677e65d376
Colm Baston <colm@colmbaston.uk>
2023-12-09 15:38:56 +0000

    2023 puzzle 09

--- 2023/puzzle09/src/main.rs
+++ 2023/puzzle09/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(array_windows)]
+
+fn main()
+{
+    let mut seqs = include_str!("../input.txt").lines()
+                                               .map(|l| l.split_whitespace()
+                                                         .map(|k| k.parse::<i32>().unwrap())
+                                                         .collect::<Vec<i32>>())
+                                               .collect::<Vec<Vec<i32>>>();
+
+    println!("{}", seqs.iter().map(|s| next_value(s)).sum::<i32>());
+    seqs.iter_mut().for_each(|s| s.reverse());
+    println!("{}", seqs.iter().map(|s| next_value(s)).sum::<i32>());
+}
+
+fn next_value(seq : &[i32]) -> i32
+{
+    if seq.iter().all(|&k| k == 0) { return 0 }
+
+    seq.last().unwrap() + next_value(&seq.array_windows()
+                                         .map(|[a, b]| b - a)
+                                         .collect::<Vec<i32>>())
 }
