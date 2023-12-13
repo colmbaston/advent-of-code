5f70ea37ce84356b741bb074e4323db57db00ae1
Colm Baston <colm@colmbaston.uk>
2023-12-13 18:49:41 +0000

    2023 puzzle 13

--- 2023/puzzle13/src/main.rs
+++ 2023/puzzle13/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    let grids = include_str!("../input.txt").split("\n\n").map(parse_grid).collect::<Vec<Grid>>();
+    println!("{}", grids.iter().filter_map(|g| summary(0, g)).sum::<usize>());
+    println!("{}", grids.iter().filter_map(|g| summary(1, g)).sum::<usize>());
+}
+
+type Grid = Vec<Vec<bool>>;
+
+fn parse_grid(s : &str) -> Grid
+{
+    s.lines()
+     .map(|l| l.bytes()
+               .map(|b| b == b'#')
+               .collect())
+     .collect()
+}
+
+fn summary(smudges : usize, grid : &Grid) -> Option<usize>
+{
+    reflect_h(smudges, grid).map(|rows| 100 * rows)
+                            .or_else(|| reflect_v(smudges, grid))
+}
+
+fn reflect_h(smudges : usize, grid : &Grid) -> Option<usize>
+{
+    (1 .. grid.len()).find(|&row| smudges == grid[.. row].iter().rev()
+                                                         .zip(grid[row ..].iter())
+                                                         .map(|(a, b)| a.iter().zip(b.iter())
+                                                                        .filter(|(c, d)| c != d)
+                                                                        .count())
+                                                         .sum())
+}
+
+fn reflect_v(smudges : usize, grid : &Grid) -> Option<usize>
+{
+    reflect_h(smudges, &aoc::transpose::transpose(grid.iter().map(Vec::as_slice)).collect::<Grid>())
 }
