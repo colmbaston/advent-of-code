e7a39e62345feb3a50ad67c5b0b991a8a7ee9a5f
Colm Baston <colm@colmbaston.uk>
2023-12-21 18:11:05 +0000

    solve part two quadratic programatically

--- 2023/puzzle21/src/main.rs
+++ 2023/puzzle21/src/main.rs
@@ -1,9 +1,10 @@
+#![feature(slice_first_last_chunk)]
 use std::collections::HashSet;
 use aoc::direction::Direction;
 
 fn main()
 {
-    let (start, walls, width, height) = parse_grid(include_str!("../input.txt"));
+    let (start, grid, width, height) = parse_grid(include_str!("../input.txt"));
 
     let mut current  = std::iter::once(start).collect::<HashSet<Pos>>();
     let mut buffer   = Vec::new();
@@ -18,54 +19,46 @@ fn main()
 
         if step % width == start.0
         {
-            sequence.push((step, current.len()));
-            if sequence.len() >= 3 { break }
+            sequence.push(current.len() as u64);
+            if sequence.len() == 3 { break }
         }
 
         buffer.extend(current.drain());
         for pos in buffer.drain(..).flat_map(|pos| Direction::ELEMS.into_iter().map(move |dir| dir.step(pos)))
         {
-            if !walls.contains(&(pos.0.rem_euclid(width), pos.1.rem_euclid(height)))
+            if grid[pos.1.rem_euclid(height) as usize][pos.0.rem_euclid(width) as usize]
             {
                 current.insert(pos);
             }
         }
     }
 
-    for (x, y) in sequence.into_iter()
-    {
-        eprintln!("f({x}) = {y}")
-    }
-
-    // hard-coded for my input for now
-    let quadratic = |x : u64| (15_350*x*x + 30_415*x - 160_240) / 17_161;
-    println!("{}", quadratic(26_501_365));
+    let x         = 26_501_365 / width as u64;
+    let [a, b, c] = sequence.last_chunk().unwrap();
+    println!("{}", a+x*(b-a+(x-1)*((a+c)/2-b)))
 }
 
 type Pos = (i32, i32);
 
-fn parse_grid(s : &str) -> (Pos, HashSet<Pos>, i32, i32)
+fn parse_grid(s : &str) -> (Pos, Vec<Vec<bool>>, i32, i32)
 {
     let mut start  = None;
-    let mut walls  = HashSet::new();
+    let mut grid   = Vec::new();
     let mut width  = 0;
     let mut height = 0;
 
     for (l, y) in s.lines().zip(0..)
     {
+        let mut row = Vec::new();
         for (b, x) in l.bytes().zip(0..)
         {
-            match b
-            {
-                b'S' => start = Some((x, y)),
-                b'.' => (),
-                b'#' => { walls.insert((x, y)); },
-                _    => unreachable!()
-            }
+            row.push(b != b'#');
+            if b == b'S' { start = Some((x, y)) }
             width = width.max(x+1);
         }
-        height = height.max(y+1);
+        grid.push(row);
+        height = height.max(y+1)
     }
 
-    (start.unwrap(), walls, width, height)
+    (start.unwrap(), grid, width, height)
 }
