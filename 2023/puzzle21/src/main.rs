a94a39cfdb64bf88a9b49027c85ceb81f1e312a2
Colm Baston <colm@colmbaston.uk>
2023-12-21 16:28:18 +0000

    2023 puzzle 21, part two, hard-coded

--- 2023/puzzle21/src/main.rs
+++ 2023/puzzle21/src/main.rs
@@ -3,35 +3,54 @@ use aoc::direction::Direction;
 
 fn main()
 {
-    let (start, width, height, walls) = parse_grid(include_str!("../input.txt"));
+    let (start, walls, width, height) = parse_grid(include_str!("../input.txt"));
 
-    let mut buffer  = Vec::new();
-    let mut current = HashSet::new();
-    current.insert(start);
+    let mut current  = std::iter::once(start).collect::<HashSet<Pos>>();
+    let mut buffer   = Vec::new();
+    let mut sequence = Vec::new();
 
-    for _ in 0 .. 64
+    for step in 0 ..
     {
-        buffer.extend(current.drain());
-        for (dir, pos) in buffer.drain(..).flat_map(|pos| Direction::ELEMS.into_iter().map(move |dir| (dir, pos)))
+        if step == 64
         {
-            let pos = dir.step(pos);
-            if (0 .. width).contains(&pos.0) && (0 .. height).contains(&pos.1) && !walls.contains(&pos)
+            println!("{}", current.len())
+        }
+
+        if step % width == start.0
+        {
+            sequence.push((step, current.len()));
+            if sequence.len() >= 3 { break }
+        }
+
+        buffer.extend(current.drain());
+        for pos in buffer.drain(..).flat_map(|pos| Direction::ELEMS.into_iter().map(move |dir| dir.step(pos)))
+        {
+            if !walls.contains(&(pos.0.rem_euclid(width), pos.1.rem_euclid(height)))
             {
                 current.insert(pos);
             }
         }
     }
-    println!("{}", current.len());
+
+    for (x, y) in sequence.into_iter()
+    {
+        eprintln!("f({x}) = {y}")
+    }
+
+    // hard-coded for my input for now
+    let quadratic = |x : u64| (15_350*x*x + 30_415*x - 160_240) / 17_161;
+    println!("{}", quadratic(26_501_365));
 }
 
 type Pos = (i32, i32);
 
-fn parse_grid(s : &str) -> (Pos, i32, i32, HashSet<Pos>)
+fn parse_grid(s : &str) -> (Pos, HashSet<Pos>, i32, i32)
 {
     let mut start  = None;
+    let mut walls  = HashSet::new();
     let mut width  = 0;
     let mut height = 0;
-    let mut walls  = HashSet::new();
+
     for (l, y) in s.lines().zip(0..)
     {
         for (b, x) in l.bytes().zip(0..)
@@ -47,5 +66,6 @@ fn parse_grid(s : &str) -> (Pos, i32, i32, HashSet<Pos>)
         }
         height = height.max(y+1);
     }
-    (start.unwrap(), width, height, walls)
+
+    (start.unwrap(), walls, width, height)
 }
