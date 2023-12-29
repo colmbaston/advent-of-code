dafec296c5b39f253f1c11e99c7a86b1931f00fe
Colm Baston <colm@colmbaston.uk>
2023-12-29 07:33:42 +0000

    divide rows by gcd to keep sizes inside i128

--- 2023/puzzle24/src/main.rs
+++ 2023/puzzle24/src/main.rs
@@ -1,6 +1,5 @@
 #![feature(iter_next_chunk)]
 use std::ops::RangeInclusive;
-use num::{ BigInt, Zero };
 
 fn main()
 {
@@ -27,14 +26,14 @@ fn main()
     let Hailstone { pos: Vec3 { x: pbx, y: pby, z: pbz }, vel: Vec3 { x: vbx, y: vby, z: vbz }} = hailstones[1];
     let Hailstone { pos: Vec3 { x: pcx, y: pcy, z: pcz }, vel: Vec3 { x: vcx, y: vcy, z: vcz }} = hailstones[2];
 
-    let mut matrix = [[0, vbz - vaz, vay - vby, 0, paz - pbz, pby - pay, vay * paz - pay * vaz + pby * vbz - vby * pbz].map(|k| k.into()).to_vec(),
-                      [0, vcz - vaz, vay - vcy, 0, paz - pcz, pcy - pay, vay * paz - pay * vaz + pcy * vcz - vcy * pcz].map(|k| k.into()).to_vec(),
-                      [vaz - vbz, 0, vbx - vax, pbz - paz, 0, pax - pbx, pax * vaz - vax * paz + vbx * pbz - pbx * vbz].map(|k| k.into()).to_vec(),
-                      [vaz - vcz, 0, vcx - vax, pcz - paz, 0, pax - pcx, pax * vaz - vax * paz + vcx * pcz - pcx * vcz].map(|k| k.into()).to_vec(),
-                      [vby - vay, vax - vbx, 0, pay - pby, pbx - pax, 0, vax * pay - pax * vay + pbx * vby - vbx * pby].map(|k| k.into()).to_vec(),
-                      [vcy - vay, vax - vcx, 0, pay - pcy, pcx - pax, 0, vax * pay - pax * vay + pcx * vcy - vcx * pcy].map(|k| k.into()).to_vec()];
+    let mut matrix = [[0, vbz - vaz, vay - vby, 0, paz - pbz, pby - pay, vay * paz - pay * vaz + pby * vbz - vby * pbz].map(|k| k as i128).to_vec(),
+                      [0, vcz - vaz, vay - vcy, 0, paz - pcz, pcy - pay, vay * paz - pay * vaz + pcy * vcz - vcy * pcz].map(|k| k as i128).to_vec(),
+                      [vaz - vbz, 0, vbx - vax, pbz - paz, 0, pax - pbx, pax * vaz - vax * paz + vbx * pbz - pbx * vbz].map(|k| k as i128).to_vec(),
+                      [vaz - vcz, 0, vcx - vax, pcz - paz, 0, pax - pcx, pax * vaz - vax * paz + vcx * pcz - pcx * vcz].map(|k| k as i128).to_vec(),
+                      [vby - vay, vax - vbx, 0, pay - pby, pbx - pax, 0, vax * pay - pax * vay + pbx * vby - vbx * pby].map(|k| k as i128).to_vec(),
+                      [vcy - vay, vax - vcx, 0, pay - pcy, pcx - pax, 0, vax * pay - pax * vay + pcx * vcy - vcx * pcy].map(|k| k as i128).to_vec()];
 
-    println!("{}", gaussian_elimination(&mut matrix).unwrap().into_iter().take(3).sum::<BigInt>())
+    println!("{}", gaussian_elimination(&mut matrix).unwrap().into_iter().take(3).sum::<i128>())
 }
 
 struct Hailstone
@@ -88,7 +87,7 @@ impl Vec3
     }
 }
 
-fn gaussian_elimination(matrix : &mut [Vec<BigInt>]) -> Option<Vec<BigInt>>
+fn gaussian_elimination(matrix : &mut [Vec<i128>]) -> Option<Vec<i128>>
 {
     let rows = matrix.len();
     if matrix.iter().any(|row| row.len() != rows+1) { return None }
@@ -96,22 +95,35 @@ fn gaussian_elimination(matrix : &mut [Vec<BigInt>]) -> Option<Vec<BigInt>>
     // convert to row echelon form
     for pivot in 0 .. rows
     {
-        matrix.swap(pivot, (pivot .. rows).find(|&i| matrix[i][pivot] != BigInt::zero())?);
+        // choose minimum |pivot| to minimise size after multiplication step
+        matrix.swap(pivot, (pivot .. rows).map(|i| (matrix[i][pivot].abs(), i))
+                                          .filter(|(k, _)| *k != 0)
+                                          .min()?.1);
+
         let (above, below) = matrix.split_at_mut(pivot+1);
 
         let row_a = above.last_mut()?;
         for row_b in below.iter_mut()
         {
-            let pivot_b = row_b[pivot].clone();
-            if  pivot_b == BigInt::zero() { continue }
-            let pivot_a = row_a[pivot].clone();
+            let pivot_b = row_b[pivot];
+            if  pivot_b == 0 { continue }
+            let pivot_a = row_a[pivot];
+
+            let mut gcd_a = 0;
+            let mut gcd_b = 0;
 
             for (a, b) in row_a[pivot..].iter_mut().zip(row_b[pivot..].iter_mut())
             {
-                *a *= &pivot_b;
-                *b *= &pivot_a;
-                *b -= &*a;
+                *a = a.checked_mul(pivot_b)?;
+                *b = b.checked_mul(pivot_a)?.checked_sub(*a)?;
+
+                gcd_a = gcd(gcd_a, *a);
+                gcd_b = gcd(gcd_b, *b);
             }
+
+            // divide rows by their gcd to reduce size for future iterations
+            if gcd_a != 0 { row_a[pivot..].iter_mut().for_each(|a| *a /= gcd_a) }
+            if gcd_b != 0 { row_b[pivot..].iter_mut().for_each(|b| *b /= gcd_b) }
         }
     }
 
@@ -119,12 +131,18 @@ fn gaussian_elimination(matrix : &mut [Vec<BigInt>]) -> Option<Vec<BigInt>>
     let mut solution = Vec::with_capacity(rows);
     for (pivot, row) in matrix.iter().enumerate().rev()
     {
-        let (sum, coeffs) = row.split_last()?;
-        let mut sum = sum.clone();
-
-        coeffs.iter().rev().zip(solution.iter()).for_each(|(c, v)| sum -= c * v);
-        solution.push(sum.checked_div(&row[pivot])?)
+        let (&(mut sum), coeffs) = row.split_last()?;
+        for (c, v) in coeffs.iter().rev().zip(solution.iter())
+        {
+            sum = sum.checked_sub(c.checked_mul(*v)?)?
+        }
+        solution.push(sum.checked_div(row[pivot])?)
     }
     solution.reverse();
     Some(solution)
+}
+
+fn gcd(a : i128, b : i128) -> i128
+{
+    if let 0 = a { b } else { gcd(b % a, a) }
 }
