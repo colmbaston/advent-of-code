22ade12e3832ec58f42cc5ff5be4dd09913c5c68
Colm Baston <colm@colmbaston.uk>
2024-01-03 16:15:50 +0000

    now using Karger

--- 2023/puzzle25/src/main.rs
+++ 2023/puzzle25/src/main.rs
@@ -1,42 +1,29 @@
-use std::{ hash::Hash, collections::{ HashMap, HashSet, VecDeque }};
+use std::{ hash::Hash, collections::HashMap };
 
 fn main()
 {
-    let mut graph   = parse_graph(include_str!("../input.txt"));
-    let mut hist    = HashMap::new();
-    let mut visited = HashSet::new();
-    let mut queue   = VecDeque::new();
+    let graph       = parse_graph(include_str!("../input.txt"));
+    let mut mapping = HashMap::new();
 
-    for &u in graph.keys()
+    loop
     {
-        visited.clear();
-        queue.push_back(u);
+        let mut graph = graph.clone();
+        mapping.clear();
+        karger(&mut graph, &mut mapping);
 
-        while let Some(v) = queue.pop_front()
-        {
-            for &w in graph[v].iter()
-            {
-                if !visited.insert(w) { continue }
-
-                *hist.entry((v.min(w), v.max(w))).or_insert(0) += 1;
-                queue.push_back(w)
-            }
-        }
+        if graph.values().next().and_then(|v| v.values().next()) == Some(&3) { break }
     }
 
-    let mut hist = hist.into_iter().collect::<Vec<((&str, &str), usize)>>();
-    hist.sort_unstable_by(|(_, a), (_, b)| b.cmp(a));
-
-    for ((u, v), _) in hist.into_iter().take(3)
+    let mut partition = HashMap::new();
+    for mut k in graph.keys()
     {
-        graph.entry(u).and_modify(|ws| { ws.remove(v); });
-        graph.entry(v).and_modify(|ws| { ws.remove(u); });
+        while let Some(l) = mapping.get(k) { k = l }
+        *partition.entry(k).or_insert(0) += 1;
     }
-
-    println!("{}", connected_components(&graph).product::<usize>());
+    println!("{}", partition.values().product::<u32>());
 }
 
-type Graph<T> = HashMap<T, HashSet<T>>;
+type Graph<T> = HashMap<T, HashMap<T, u32>>;
 
 fn parse_graph(s : &str) -> Graph<&str>
 {
@@ -46,24 +33,38 @@ fn parse_graph(s : &str) -> Graph<&str>
         let (u, vs) = l.split_once(": ").unwrap();
         for v in vs.split_whitespace()
         {
-            graph.entry(u).or_insert_with(HashSet::new).insert(v);
-            graph.entry(v).or_insert_with(HashSet::new).insert(u);
+            *graph.entry(u).or_insert_with(HashMap::new).entry(v).or_insert(0) += 1;
+            *graph.entry(v).or_insert_with(HashMap::new).entry(u).or_insert(0) += 1;
         }
     }
     graph
 }
 
-fn connected_components<T : Copy + Eq + Hash>(graph : &Graph<T>) -> impl Iterator<Item = usize> + '_
+fn karger<T : Copy + Eq + Hash>(graph : &mut Graph<T>, mapping : &mut HashMap<T, T>)
 {
-    let mut visited = HashSet::new();
-    graph.keys()
-         .map(move |&vertex| dfs(vertex, graph, &mut visited))
-         .filter(|&size| size > 0)
+    use rand::seq::{ IteratorRandom, SliceRandom };
+    let mut rng = rand::thread_rng();
+
+    for _ in 2 .. graph.len()
+    {
+        let (&u, vs) = graph.iter().choose(&mut rng).unwrap();
+        let  &v      = vs.iter().collect::<Vec<_>>().choose_weighted(&mut rng, |(_, &k)| k).unwrap().0;
+
+        contract(u, v, graph);
+        mapping.insert(v, u);
+    }
 }
 
-fn dfs<T : Copy + Eq + Hash>(vertex : T, graph : &Graph<T>, visited : &mut HashSet<T>) -> usize
+fn contract<T : Copy + Eq + Hash>(u : T, v: T, graph : &mut Graph<T>)
 {
-    if !visited.insert(vertex) { return 0 }
+    graph.get_mut(&u).unwrap().remove(&v);
+    let mut v_adj = graph.remove(&v).unwrap();
+    v_adj.remove(&u);
 
-    1 + graph[&vertex].iter().map(|&v| dfs(v, graph, visited)).sum::<usize>()
+    for (w, j) in v_adj.into_iter()
+    {
+        *graph.entry(u).or_default().entry(w).or_insert(0) += j;
+        let w_adj = graph.entry(w).or_default();
+        if let Some(k) = w_adj.remove(&v) { *w_adj.entry(u).or_insert(0) += k }
+    }
 }
