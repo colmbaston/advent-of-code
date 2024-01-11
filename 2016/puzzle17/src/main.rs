6f9785aea79251c219cac0b1c703273756738532
Colm Baston <colm@colmbaston.uk>
2024-01-11 20:08:52 +0000

    2016 puzzle 17, part two

--- 2016/puzzle17/src/main.rs
+++ 2016/puzzle17/src/main.rs
@@ -1,39 +1,47 @@
 use md5::{ Md5, Digest };
 use aoc::direction::Direction;
-use std::collections::VecDeque;
 
 fn main()
 {
-    let input = include_str!("../input.txt").trim_end();
+    let mut path     = Vec::new();
+    let mut shortest = None;
+    let mut longest  = 0;
+    dfs(include_str!("../input.txt").trim_end(), (0, 0), &mut path, &mut shortest, &mut longest);
+    println!("{}", std::str::from_utf8(&shortest.unwrap()).unwrap());
+    println!("{longest}");
+}
 
-    let mut queue = VecDeque::new();
-    queue.push_back(((0, 0), String::new()));
+type Pos = (u8, u8);
 
-    while let Some((pos, path)) = queue.pop_front()
+fn dfs(input : &str, pos : Pos, path : &mut Vec<u8>, shortest : &mut Option<Vec<u8>>, longest : &mut usize)
+{
+    if pos == (3, 3)
     {
-        if pos == (3, 3)
-        {
-            println!("{path}");
-            break
-        }
-
+        *longest = path.len().max(*longest);
+        if shortest.as_ref().filter(|s| s.len() <= path.len()).is_none() { *shortest = Some(path.clone()) }
+    }
+    else
+    {
         let mut hasher = Md5::new();
         hasher.update(input);
         hasher.update(&path);
         let hash = hasher.finalize();
 
-        queue.extend([(Direction::North, hash[0] >>  4),
-                      (Direction::South, hash[0] & 0xf),
-                      (Direction::West,  hash[1] >>  4),
-                      (Direction::East,  hash[1] & 0xf)].into_iter()
-                                                        .filter(|(_, b)| *b > 0xa)
-                                                        .filter_map(|(dir, _)| dir.checked_step(pos).map(|pos| (pos, dir)))
-                                                        .filter(|((x, y), _)| *x <= 3 && *y <= 3)
-                                                        .map(|(pos, dir)| (pos, { let mut path = path.clone(); path.push(format_dir(dir) as char); path })))
+        for (dir, pos) in Direction::ELEMS.into_iter()
+                                          .zip([hash[0] >> 4, hash[1] & 0xf, hash[0] & 0xf, hash[1] >> 4])
+                                          .filter(|(_, b)| *b > 0xa)
+                                          .filter_map(|(dir, _)| dir.checked_step(pos)
+                                                                    .filter(|&(x, y)| x <= 3 && y <= 3)
+                                                                    .map(|pos| (dir, pos)))
+        {
+            path.push(udlr(dir));
+            dfs(input, pos, path, shortest, longest);
+            path.pop();
+        }
     }
 }
 
-fn format_dir(dir : Direction) -> u8
+fn udlr(dir : Direction) -> u8
 {
     match dir
     {
