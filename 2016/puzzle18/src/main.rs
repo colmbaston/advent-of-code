2f5cfa72603a5cf06231dac57f2e263689bd3f12
Colm Baston <colm@colmbaston.uk>
2024-01-11 21:14:55 +0000

    2016 puzzle 18

--- 2016/puzzle18/src/main.rs
+++ 2016/puzzle18/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(array_windows)]
+
+fn main()
+{
+    let mut row = vec![false];
+    row.extend(include_str!("../input.txt").trim_end().bytes().map(|b| b == b'^'));
+    row.push(false);
+
+    let mut count  = 0;
+    let mut buffer = Vec::new();
+    for cycle in 0 ..
+    {
+        if      cycle ==      40 { println!("{count}")        }
+        else if cycle == 400_000 { println!("{count}"); break }
+
+        buffer.clear();
+        buffer.push(false);
+        buffer.extend(row.array_windows().map(|&[a, b, c]| { count += !b as u32; a ^ c }));
+        buffer.push(false);
+
+        std::mem::swap(&mut row, &mut buffer);
+    }
 }
