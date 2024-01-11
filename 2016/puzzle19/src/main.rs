c6d73c092ab8376bc14a6b12b72253e5d9bf12b2
Colm Baston <colm@colmbaston.uk>
2024-01-11 21:56:48 +0000

    2016 puzzle 19

--- 2016/puzzle19/src/main.rs
+++ 2016/puzzle19/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    let input = include_str!("../input.txt").trim_end().parse().unwrap();
+    println!("{}", josephus_one(input));
+    println!("{}", josephus_two(input));
+}
+
+fn josephus_one(n : u32) -> u32
+{
+    (n - (1 << n.ilog(2))) << 1 | 1
+}
+
+fn josephus_two(n : u32) -> u32
+{
+    let pow = 3u32.pow(n.ilog(3));
+    if n == pow
+    {
+        pow
+    }
+    else if n - pow <= pow
+    {
+        n - pow
+    }
+    else
+    {
+        2 * n - 3 * pow
+    }
 }
