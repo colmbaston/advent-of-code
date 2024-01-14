1e37d250d240c59c4fd4522e0920236f08baeb3c
Colm Baston <colm@colmbaston.uk>
2024-01-14 20:30:32 +0000

    2017 puzzle 04

--- 2017/puzzle04/src/main.rs
+++ 2017/puzzle04/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(array_windows)]
+
+fn main()
+{
+    let mut phrases = include_str!("../input.txt").lines()
+                                                  .map(|l| l.split_whitespace()
+                                                            .map(|w| w.as_bytes().to_vec())
+                                                            .collect::<Vec<Vec<u8>>>())
+                                                  .collect::<Vec<Vec<Vec<u8>>>>();
+
+    let mut count = 0;
+    for phrase in phrases.iter_mut()
+    {
+        phrase.sort_unstable();
+        count += phrase.array_windows().all(|[a, b]| a != b) as u32;
+    }
+    println!("{count}");
+
+    count = 0;
+    for phrase in phrases.iter_mut()
+    {
+        phrase.iter_mut().for_each(|word| word.sort_unstable());
+        phrase.sort_unstable();
+        count += phrase.array_windows().all(|[a, b]| a != b) as u32;
+    }
+    println!("{count}");
 }
