a39d068e0290e433f166065cf40e5d420b06cbef
Colm Baston <colm@colmbaston.uk>
2024-01-14 20:37:46 +0000

    2017 puzzle 05

--- 2017/puzzle05/src/main.rs
+++ 2017/puzzle05/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    let input = include_str!("../input.txt").lines()
+                                            .map(|k| k.parse::<i32>().unwrap())
+                                            .collect::<Vec<i32>>();
+
+    let mut pc    = 0;
+    let mut steps = 0;
+    let mut jumps = input.clone();
+    while let Some(j) = pc.try_into().ok().and_then(|i : usize| jumps.get_mut(i))
+    {
+        pc    += *j;
+        *j    += 1;
+        steps += 1
+    }
+    println!("{steps}");
+
+    pc    = 0;
+    steps = 0;
+    jumps = input;
+    while let Some(j) = pc.try_into().ok().and_then(|i : usize| jumps.get_mut(i))
+    {
+        pc    += *j;
+        *j    += if *j >= 3 { -1 } else { 1 };
+        steps += 1
+    }
+    println!("{steps}");
 }
