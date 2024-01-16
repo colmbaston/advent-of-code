06b3830557d5f68fc836215e8083e3f56b94f7d1
Colm Baston <colm@colmbaston.uk>
2024-01-16 15:18:29 +0000

    2017 puzzle 09

--- 2017/puzzle09/src/main.rs
+++ 2017/puzzle09/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    let mut score   = 0;
+    let mut count   = 0;
+    let mut depth   = 0;
+    let mut garbage = false;
+    let mut negate  = false;
+
+    for b in include_str!("../input.txt").trim_end().bytes()
+    {
+        if !garbage
+        {
+            match b
+            {
+                b'{' =>                   depth += 1,
+                b'}' => { score += depth; depth -= 1 },
+                b'<' => garbage = true,
+                _    => ()
+            }
+        }
+        else if !std::mem::replace(&mut negate, false)
+        {
+            match b
+            {
+                b'>' => garbage = false,
+                b'!' => negate  = true,
+                _    => count  += 1
+            }
+        }
+    }
+
+    println!("{score}");
+    println!("{count}");
 }
