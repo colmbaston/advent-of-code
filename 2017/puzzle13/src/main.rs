8f3d93cfddf610a708591bf22276031008fc2a8b
Colm Baston <colm@colmbaston.uk>
2024-01-17 21:00:22 +0000

    2017 puzzle 13

--- 2017/puzzle13/src/main.rs
+++ 2017/puzzle13/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    let input = include_str!("../input.txt").lines().map(parse_line).collect::<Vec<(u32, u32)>>();
+    println!("{}", input.iter().map(|(depth, range)| if depth % (2 * (range-1)) == 0 { depth * range } else { 0 }).sum::<u32>());
+    println!("{}", (0 ..).find(|delay| input.iter().all(|(depth, range)| (depth + delay) % (2 * (range - 1)) != 0)).unwrap());
+}
+
+fn parse_line(s : &str) -> (u32, u32)
+{
+    let (a, b) = s.split_once(": ").unwrap();
+    (a.parse().unwrap(), b.parse().unwrap())
 }
