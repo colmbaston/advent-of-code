7421de09f146928a61bdc6cbf8374e72f5a859e6
Colm Baston <colm@colmbaston.uk>
2024-01-18 15:03:45 +0000

    2017 puzzle 17, brute force

--- 2017/puzzle17/src/main.rs
+++ 2017/puzzle17/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::VecDeque;
+
+fn main()
+{
+    let input      = include_str!("../input.txt").trim_end().parse::<usize>().unwrap();
+    let mut buffer = std::iter::once(0).collect::<VecDeque<u32>>();
+    for k in 1 ..= 50_000_000
+    {
+        buffer.rotate_left((input+1) % buffer.len());
+        buffer.push_front(k);
+
+        if k == 2017 { println!("{}", buffer[1]) }
+    }
+    let zero = buffer.iter().position(|&k| k == 0).unwrap();
+    println!("{}", buffer[(zero + 1) % buffer.len()]);
 }
