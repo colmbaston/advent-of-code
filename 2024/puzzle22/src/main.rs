e8eebd3f4a45a185673a9f1357363f54395c8ed8
Colm Baston <colm@colmbaston.uk>
2024-12-22 14:10:39 +0000

    don't allocate vecs, no longer using nested hashmap

--- 2024/puzzle22/src/main.rs
+++ 2024/puzzle22/src/main.rs
@@ -2,43 +2,36 @@ use std::collections::HashMap;
 
 fn main()
 {
-    let numbers = include_str!("../input.txt").lines()
-                                              .map(|l| Prng { state: l.parse().unwrap() }.take(2001).collect::<Vec<u64>>())
-                                              .collect::<Vec<Vec<u64>>>();
-
-    println!("{}", numbers.iter().map(|seq| seq[2000]).sum::<u64>());
-
+    let mut sum     = 0;
+    let mut max     = 0;
     let mut windows = HashMap::new();
-    for (seller, sequence) in numbers.into_iter().enumerate()
+    for (mut state, seller) in include_str!("../input.txt").lines().map(|l| l.parse::<u64>().unwrap()).zip(1 ..)
     {
-        let prices  = sequence.into_iter().map(|k| (k % 10) as i8).collect::<Vec<i8>>();
-        let changes = prices.windows(2).map(|w| w[1] - w[0]).collect::<Vec<i8>>();
-
-        for (w, &price) in changes.windows(4).zip(prices.iter().skip(4))
+        let mut window = 0;
+        let mut prev   = (state % 10) as u32;
+        for i in 1 ..= 2000
         {
-            windows.entry((w[0], w[1], w[2], w[3]))
-                   .or_insert_with(HashMap::new)
-                   .entry(seller)
-                   .or_insert(price);
+            const MASK : u64 = (1 << 24) - 1;
+            state = ((state <<  6) ^ state) & MASK;
+            state = ((state >>  5) ^ state) & MASK;
+            state = ((state << 11) ^ state) & MASK;
+
+            let price = (state % 10) as u32;
+            window    = window << 8 | (9+price-prev);
+            prev      = price;
+
+            if i >= 4
+            {
+                let (a, b) = windows.entry(window).or_insert((0, 0));
+                if std::mem::replace(a, seller) != seller
+                {
+                    *b += price;
+                    max = max.max(*b);
+                }
+            }
         }
+        sum += state;
     }
-    println!("{}", windows.into_values().map(|m| m.into_values().map(|v| v as u32).sum::<u32>()).max().unwrap());
-}
-
-struct Prng { state: u64 }
-
-impl Iterator for Prng
-{
-    type Item = u64;
-
-    fn next(&mut self) -> Option<u64>
-    {
-        const MASK : u64 = (1 << 24) - 1;
-
-        let result = self.state;
-        self.state = ((self.state <<  6) ^ self.state) & MASK;
-        self.state = ((self.state >>  5) ^ self.state) & MASK;
-        self.state = ((self.state << 11) ^ self.state) & MASK;
-        Some(result)
-    }
+    println!("{sum}");
+    println!("{max}");
 }
