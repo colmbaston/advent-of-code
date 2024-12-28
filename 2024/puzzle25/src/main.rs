fb138374b529ebe76c4dceb94ef5a90c398945bf
Colm Baston <colm@colmbaston.uk>
2024-12-28 09:46:44 +0000

    represent locks and keys bitwise

--- 2024/puzzle25/src/main.rs
+++ 2024/puzzle25/src/main.rs
@@ -1,37 +1,12 @@
 fn main()
 {
-    let mut locks = Vec::new();
-    let mut keys  = Vec::new();
-    for (is_lock, heights) in include_str!("../input.txt").split("\n\n").map(parse_schematic)
-    {
-        if is_lock { locks.push(heights) } else { keys.push(heights) }
-    }
-
-    let mut count = 0;
-    for lock in locks.into_iter()
-    {
-        for key in keys.iter()
-        {
-            count += lock.iter().zip(key.iter()).all(|(l, k)| l + k <= 7) as u32
-        }
-    }
-    println!("{count}");
+    let (locks, keys) : (Vec<u64>, Vec<u64>) = include_str!("../input.txt").split("\n\n").map(parse_schematic).partition(|s| s & 1 == 1);
+    println!("{}", locks.iter().flat_map(|l| keys.iter().map(move |k| (l, k))).filter(|&(l, k)| l & k == 0).count());
 }
 
-fn parse_schematic(s : &str) -> (bool, [u8 ; 5])
+fn parse_schematic(s : &str) -> u64
 {
-    let is_lock = s.as_bytes()[0] == b'#';
-    let mut lines = s.lines().map(|l| l.as_bytes()).collect::<Vec<&[u8]>>();
-    if is_lock { lines.reverse() }
-
-    let mut heights = [0 ; 5];
-    for l in lines.into_iter()
-    {
-        for (&b, h) in l.iter().zip(heights.iter_mut())
-        {
-            if b == b'#' { *h += 1 }
-        }
-    }
-
-    (is_lock, heights)
+    s.lines()
+     .flat_map(str::bytes)
+     .fold(0, |a, b| (a << 1) | (b == b'#') as u64)
 }
