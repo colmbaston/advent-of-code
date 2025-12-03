d6dba97ab5a2c9f75c3c1dd9470b3c7b3b59c2c9
Colm Baston <colm@colmbaston.uk>
2025-12-03 14:34:37 +0000

    2025 puzzle 03

--- 2025/puzzle03/src/main.rs
+++ 2025/puzzle03/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    let mut sum_one = 0;
+    let mut sum_two = 0;
+    for bank in include_str!("../input.txt").lines().map(|l| l.bytes().map(|b| b - b'0').collect::<Vec<u8>>())
+    {
+        sum_one += best_joltage(&bank,  2);
+        sum_two += best_joltage(&bank, 12);
+    }
+    println!("{sum_one}");
+    println!("{sum_two}");
+}
+
+fn best_joltage(bank : &[u8], size : usize) -> u64
+{
+    let mut best = bank[.. size].to_vec();
+    for &joltage in &bank[size ..]
+    {
+        best.push(joltage);
+        for i in 0 .. size
+        {
+            if best[i] < best[i+1]
+            {
+                best.remove(i);
+                break
+            }
+        }
+        if best.len() > size { best.pop(); }
+    }
+    best.into_iter().fold(0, |a, j| a*10 + j as u64)
 }
