e02d33ad069e7c9ea1bc9e93bb939f17994af516
Colm Baston <colm@colmbaston.uk>
2025-12-07 15:51:47 +0000

    2025 puzzle 07

--- 2025/puzzle07/src/main.rs
+++ 2025/puzzle07/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+fn main()
+{
+    let grid = include_str!("../input.txt").lines()
+                                           .map(|l| l.as_bytes())
+                                           .collect::<Vec<&[u8]>>();
+
+    let mut splits = 0;
+    let mut beams  = HashMap::new();
+    let mut next   = HashMap::new();
+    beams.insert(grid[0].iter().position(|&b| b == b'S').unwrap(), 1);
+    for row in grid.iter()
+    {
+        for (beam, count) in beams.drain()
+        {
+            if let b'^' = row[beam]
+            {
+                splits += 1;
+                *next.entry(beam-1).or_insert(0) += count;
+                *next.entry(beam+1).or_insert(0) += count;
+            }
+            else
+            {
+                *next.entry(beam).or_insert(0) += count;
+            }
+        }
+        std::mem::swap(&mut beams, &mut next);
+    }
+    println!("{splits}");
+    println!("{}", beams.into_values().sum::<u64>());
 }
