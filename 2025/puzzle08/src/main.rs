d05881c02dada2c1ef0a395a45a9e3dbc544d935
Colm Baston <colm@colmbaston.uk>
2025-12-08 18:04:50 +0000

    use Vec<Point> to store circuit rather than HashSet<Point>

--- 2025/puzzle08/src/main.rs
+++ 2025/puzzle08/src/main.rs
@@ -1,8 +1,9 @@
-use std::collections::{ HashMap, HashSet };
+use std::collections::HashMap;
 
 fn main()
 {
     let points    = include_str!("../input.txt").lines().map(Point::parse).collect::<Vec<Point>>();
+
     let mut dists = Vec::new();
     for (i, &p) in points.iter().enumerate()
     {
@@ -11,10 +12,10 @@ fn main()
             dists.push((p.distance(q), p, q));
         }
     }
-    dists.sort_unstable_by(|d, e| d.0.total_cmp(&e.0));
+    dists.sort_unstable_by(|a, b| a.0.total_cmp(&b.0));
 
     let mut reps     = points.iter().map(|&p| (p, p)).collect::<HashMap<Point, Point>>();
-    let mut circuits = points.iter().map(|&p| (p, std::iter::once(p).collect())).collect::<HashMap<Point, HashSet<Point>>>();
+    let mut circuits = points.iter().map(|&p| (p, vec![p])).collect::<HashMap<Point, Vec<Point>>>();
     for (i, &(_, p, q)) in dists.iter().enumerate()
     {
         if i == 1000
@@ -23,23 +24,22 @@ fn main()
             for (rp, rq) in reps.iter()
             {
                 if rp != rq { continue }
-                sizes.push(circuits[rp].len() as u64)
+                sizes.push(circuits[rp].len() as u32)
             }
             sizes.sort_unstable();
-            println!("{}", sizes.into_iter().rev().take(3).product::<u64>());
+            println!("{}", sizes.into_iter().rev().take(3).product::<u32>());
         }
 
         let rp = reps[&p];
         let rq = reps[&q];
         if rp == rq { continue }
 
-        let union = circuits[&rp].union(&circuits[&rq]).copied().collect::<HashSet<Point>>();
-        for &r in union.iter()
+        let circuit = circuits.remove(&rq).unwrap();
+        for &r in circuit.iter()
         {
             reps.insert(r, rp);
-            circuits.remove(&r);
         }
-        circuits.insert(rp, union);
+        circuits.entry(rp).and_modify(|c| c.extend(circuit));
 
         if circuits.len() == 1
         {
